ec1bba99d1d4d5395ac8eb2802498f1f24ef8129
Monde Manzini <monde.manzini@skao.int>
2022-05-29 11:29:45 +0100

    Code cleaning, fixed string formatting.

--- sig_gen/sg_smb100a_setup_basic_params_1.py
+++ sig_gen/sg_smb100a_setup_basic_params_1.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Mon Feb 24 10:23:49 2020
 
 @author: Rishad modifed by Sias 
@@ -14,7 +14,7 @@ Created on Mon Feb 24 10:23:49 2020
         Copied Power and Frequency from the NoiseIncreaseRev1.py
         for sig gen use only
         Added funtion calls
-"""
+'''
 
 from re import S
 import time
@@ -23,169 +23,182 @@ import socket
 # The script uses raw ethernet socket communication, and thus VISA library/installation is not required
 
 # -----------Connection Settings--------------
-sigPORT = 5025             # default SMB R&S port 
-sigHOST = '10.8.88.166'    #                        -- Section to be removed to a separate file
+SIGPORT = 5025             # default SMB R&S port 
+SIGHOST = '10.8.88.166'    # IP Address of the SMB100A                    
 #---------------------------------------------
 
 
 # --------------Initialization of Variables---
 Power = 0.0                                 
-Freq = 100e3    # Minimum 100kHz     
-Zero = 0
-One = 1
+Freq = 100e3    # Minimum 9 kHz     
+ZERO = 0
+ONE = 1
 OFF = 'OFF'
 ON = 'ON'
-AM_Mod = 'AM'
-FM_Mod = 'FM'
-PM_Mod = 'PM'
-default_timeout = 1
+AM_MOD = 'AM'
+FM_MOD = 'FM'
+PM_MOD = 'PM'
+DEFAULT_TIMEOUT = 1
 # --------------------------------------------
          
 
 def initSigGen():
-    """
+    '''
     This function establishes a socket connection and identifies the instrument
     @params     : None
-    """
+    '''
     try:
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((sigHOST,sigPORT))
-        s.settimeout(default_timeout)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SIGHOST, SIGPORT))
+        s.settimeout(DEFAULT_TIMEOUT)
         if s:
-            print(s,"Connection succesful.")
+            print(s, 'Connection succesful.')
     except Exception as e:
-        print(e,"Check to see if the port number is {PORT}")
+        print(e, f'Check to see if the port number is {SIGPORT}')
     s.sendall(b'*IDN?\r\n')                             
     data = s.recv(1024)
-    state=0
-    setstate='OUTP1 {}\r\n'.format(state)       # Sets RF Output
-    s.sendall(bytes(setstate, encoding='utf8'))
+    state = 0
+    setstate = 'OUTP1 {}\r\n'.format(state)       # Sets RF Output
+    s.sendall(bytes(setstate, encoding = 'utf8'))
     s.sendall(b'OUTP1?\r\n')
-    data=s.recv(1024)
+    data = s.recv(1024)
     s.close()
-    if data.decode('utf8')=='1\n':      # 
-        print("RF Output On")
-    else: print("RF Output Off")
+    if data.decode('utf8') == '1\n':       
+        print('RF Output On')
+    else: print('RF Output Off')
 
 def setSigGenPower(power):
-    """
+    ''' Set power
+
     This function sets the power of the signal generator
-    @param  Power: float
-    """
+    @param:
+        power: float in dBm
+    '''
     try:
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((sigHOST, sigPORT))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SIGHOST, SIGPORT))
         s.settimeout(1)
     except Exception as e:
-        print(e,"Check to see if the port number is {PORT}")
+        print(e, f'Check to see if the port number is {SIGPORT}')
     setpower = 'POW {}\r\n'.format(power)
-    s.sendall(bytes(setpower, encoding='utf8'))
+    s.sendall(bytes(setpower, encoding = 'utf8'))
     s.sendall(b'POW?\r\n')
     data = float(s.recv(1024))
-    print("Sig gen power = %f dBm" %data)
+    print(f'Sig gen power = {data} dBm')
 
 def setSigGenFreq(Freq):
-    """
-    Identify instrument. Can be used as a connectivity check.
-    """
+    ''' Set frequency
+
+    This function identifies the instrument. Can be used as a connectivity check.
+    @params:
+        frequency: float in Hz
+    '''
     try:
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((sigHOST, sigPORT))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SIGHOST, SIGPORT))
         s.settimeout(1)
     except Exception as e:
-        print(e,"Check to see if the port number is {PORT}")
-    setfreq= "FREQ {}\r\n".format(Freq)
-    s.sendall(bytes(setfreq, encoding='utf8'))
+        print(e, f'Check to see if the port number is {SIGPORT}')
+    setfreq = 'FREQ {}\r\n'.format(Freq)
+    s.sendall(bytes(setfreq, encoding = 'utf8'))
     s.sendall(b'FREQ?\r\n')
     data = float(s.recv(1024))
     s.close()
-    print(f"Sig gen frequency = {(data/1e6)} MHz")
+    print(f'Sig gen frequency = {(data / 1e6)} MHz')
 
 def setSigGenState(RFOut):
-    """
+    ''' Set RF state
+
     This function turns on/off the RF output state
-    @params RFOut:  Integer
-    """
+    @params:
+        RFOut:  Integer
+    '''
     try:
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((sigHOST, sigPORT))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SIGHOST, SIGPORT))
         s.settimeout(1)
     except Exception as e:
-        print(e,"Check to see if the port number is {PORT}")
-    setstate='OUTP1 {}\r\n'.format(RFOut)
-    s.sendall(bytes(setstate, encoding='utf8'))
+        print(e, f'Check to see if the port number is {SIGPORT}')
+    setstate = 'OUTP1 {}\r\n'.format(RFOut)
+    s.sendall(bytes(setstate, encoding = 'utf8'))
     s.sendall(b'OUTP1?\r\n')
-    data=s.recv(1024)
-    print('Received', data, 'for RFOut')
+    data = s.recv(1024)
+    print(f'Received {data} for RFOut')
     s.close()
-    if data.decode("UTF-8") == "1\n":
-        print("RF Output On")
-    else: print(("RF Output Off"))
+    if data.decode('UTF-8') == '1\n':
+        print('RF Output On')
+    else: print('RF Output Off')
 
 def setSigGenModsState(ModsState):
-    """
+    ''' Set All Modulation States
+
     This function sets all modulation modes off
-    @param ModsState: String
-    """
+    @param:
+        ModsState: String On/Off
+    '''
     try:
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((sigHOST, sigPORT))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SIGHOST, SIGPORT))
         s.settimeout(1)
     except Exception as e:
-        print(e,"Check to see if the port number is {PORT}")
-    setModsState='MOD:STAT {}\r\n'.format(ModsState)
-    s.sendall(bytes(setModsState, encoding='utf8'))
+        print(e, f'Check to see if the port number is {SIGPORT}')
+    setModsState = 'MOD:STAT {}\r\n'.format(ModsState)
+    s.sendall(bytes(setModsState, encoding = 'utf8'))
     s.sendall(b'MOD:STAT?\n')
-    data=s.recv(1024)
-    print('Received', data)
+    data = s.recv(1024)
+    print(f'Received {data}')
     s.close()
-    if data.decode("UTF-8") == "0\n":
-        print("All modulations Off")
-    else: print(("All modulations On"))
+    if data.decode('UTF-8') == '0\n':
+        print('All modulations Off')
+    else: print('All modulations On')
 
 # ---------------------Modulations State Function-----------------------
 def setSigGenModState(ModState, Val):
-    """
+    ''' Set Modulation Scheme
+
     This function sets on different modulation schemes:
-    @param  AM: string /
-            PM: string /
-            FM: string
-    """
+    @param:
+        AM: string /
+        PM: string /
+        FM: string
+    '''
     try:
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((sigHOST, sigPORT))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SIGHOST, SIGPORT))
         s.settimeout(1)
     except Exception as e:
-        print(e,"Check to see if the port number is {PORT}")
-    setModState='{}:STAT {}\r\n'.format(ModState, Val)
-    s.sendall(bytes(setModState, encoding='utf8'))
-    setModState='{}:STAT?\r\n'.format(ModState)
-    s.sendall(bytes(setModState, encoding='utf8'))
-    data=s.recv(1024)
-    print('Received', data)
+        print(e, f'Check to see if the port number is {SIGPORT}')
+    setModState = '{}:STAT {}\r\n'.format(ModState, Val)
+    s.sendall(bytes(setModState, encoding = 'utf8'))
+    setModState = '{}:STAT?\r\n'.format(ModState)
+    s.sendall(bytes(setModState, encoding = 'utf8'))
+    data = s.recv(1024)
+    print(f'Received {data}')
     s.close()
-    if data.decode("UTF-8") == "1\n":
-        print(f"{ModState} Modulation On")
-    else: print(f"{ModState} Modulation On")
+    if data.decode('UTF-8') == '1\n':
+        print(f'{ModState} Modulation On')
+    else: print(f'{ModState} Modulation On')
 
 def LFOutputState(LFOState):            
-    """
+    ''' Set Low Frequency State
+
     This function sets the LFO Sweep Mode to Manual
-    @param ModsState: String
-    """
+    @params:
+        LFOState: String On/Of
+    '''
     try:
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect((sigHOST, sigPORT))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SIGHOST, SIGPORT))
         s.settimeout(1)
     except Exception as e:
-        print(e,"Check to see if the port number is {PORT}")
-    setLFOState='LFO:SWE:MODE?\r\n'
-    s.sendall(bytes(setLFOState, encoding='utf8'))
-    setLFOState='LFO {}\r\n'.format(LFOState)
-    s.sendall(bytes(setLFOState, encoding='utf8'))
+        print(e, f'Check to see if the port number is {SIGPORT}')
+    setLFOState = 'LFO:SWE:MODE?\r\n'
+    s.sendall(bytes(setLFOState, encoding = 'utf8'))
+    setLFOState = 'LFO {}\r\n'.format(LFOState)
+    s.sendall(bytes(setLFOState, encoding = 'utf8'))
     s.sendall(b'LFO:SWE:MODE MAN\n')
-    data=s.recv(1024)
-    print('LFO:SWE:MODE Received', data)
+    data = s.recv(1024)
+    print(f'LFO:SWE:MODE Received {data}')
     s.close()
     
 # ---------------------Main Function-----------------------------------
@@ -198,15 +211,15 @@ def setupSigGen():
     time.sleep(2)                   # Wait a bit
     setSigGenFreq(Freq)             # Set the power to 100kHz
     time.sleep(2)                   # Wait a bit
-    setSigGenState(Zero)            # Turn off sig gen output
+    setSigGenState(ZERO)            # Turn off sig gen output
     time.sleep(2)                   # Wait a bit
     setSigGenModsState(OFF)         # Switch all modulations off
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(AM_Mod,OFF)   # Switch AM Modulation off
+    setSigGenModState(AM_MOD, OFF)   # Switch AM Modulation off
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(FM_Mod,OFF)   # Switch AM Modulation off
+    setSigGenModState(FM_MOD, OFF)   # Switch AM Modulation off
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(PM_Mod,OFF)   # Switch AM Modulation off
+    setSigGenModState(PM_MOD, OFF)   # Switch AM Modulation off
     time.sleep(2)                   # Wait a bit
     LFOutputState(OFF)              # Switch LFO off
     time.sleep(2)                   # Wait a bit
@@ -216,17 +229,17 @@ def setupSigGen():
     time.sleep(2)                   # Wait a bit  
     setSigGenFreq(1500e6)           # Set the power to 1.5GHz
     time.sleep(2)                   # Wait a bit   
-    setSigGenState(One)             # Turn on sig gen output
+    setSigGenState(ONE)             # Turn on sig gen output
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(AM_Mod,ON)    # Switch AM Modulation on
+    setSigGenModState(AM_MOD, ON)    # Switch AM Modulation on
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(AM_Mod,OFF)   # Switch AM Modulation off
+    setSigGenModState(AM_MOD, OFF)   # Switch AM Modulation off
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(FM_Mod,ON)    # Switch FM Modulation on
+    setSigGenModState(FM_MOD, ON)    # Switch FM Modulation on
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(FM_Mod,OFF)   # Switch FM Modulation on
+    setSigGenModState(FM_MOD, OFF)   # Switch FM Modulation on
     time.sleep(2)                   # Wait a bit
-    setSigGenModState(PM_Mod,ON)    # Switch FM Modulation on
+    setSigGenModState(PM_MOD, ON)    # Switch FM Modulation on
     LFOutputState(ON)               # Switch LFO on
 
 #    ----All settings off again
@@ -235,21 +248,21 @@ def setupSigGen():
     time.sleep(5)                   # Wait a bit
     setSigGenFreq(Freq)             # Set the power to 100kHz
     time.sleep(5)                   # Wait a bit
-    setSigGenState(Zero)            # Turn off sig gen output
+    setSigGenState(ZERO)            # Turn off sig gen output
     time.sleep(5)                   # Wait a bit
     setSigGenModsState(OFF)         # Switch all modulations off
     time.sleep(5)                   # Wait a bit
-    setSigGenModState(AM_Mod,OFF)   # Switch AM Modulation off
+    setSigGenModState(AM_MOD,OFF)   # Switch AM Modulation off
     time.sleep(5)                   # Wait a bit
-    setSigGenModState(FM_Mod,OFF)   # Switch AM Modulation off
+    setSigGenModState(FM_MOD,OFF)   # Switch AM Modulation off
     time.sleep(5)                   # Wait a bit
-    setSigGenModState(PM_Mod,OFF)   # Switch AM Modulation off
+    setSigGenModState(PM_MOD,OFF)   # Switch AM Modulation off
     time.sleep(5)                   # Wait a bit    
     LFOutputState(OFF)              # Switch LFO off
-    print("/------End of Setup signal generator---------/")
+    print('/------End of Setup signal generator---------/')
 
 #%%   
 # Main program
 #-----------------------------------------------------------------------------    
-print("/------Setup signal generator---------/")
+print('/------Setup signal generator---------/')
 setupSigGen()
