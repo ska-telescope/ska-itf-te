8158f3d7c32f30a115afd253fbce861a60dd7428
Adriaan de Beer <adebeer42@gmail.com>
2023-01-31 18:18:34 +0200

    AT-331 copied back in a bunch of Makefile options from SER_TEST_EQ

--- Makefile
+++ Makefile
@@ -46,3 +46,36 @@ python-post-lint:
 	mypy --config-file mypy.ini src/ tests/
 
 .PHONY: python-post-lint
+
+DOCS_SPHINXOPTS=-n -W --keep-going
+
+# Use the previously built image when running in the pipeline
+ifneq ($(CI_JOB_ID),)
+OCI_TAG = $(VERSION)-dev.c$(CI_COMMIT_SHORT_SHA)
+CI_REGISTRY ?= registry.gitlab.com
+# For k8s-install-chart
+VALUES += --set test_equipment.image.registry=$(CI_REGISTRY)/ska-telescope/$(NAME) \
+	--set test_equipment.image.tag=$(OCI_TAG)
+# for k8s-test
+K8S_TEST_IMAGE_TO_TEST=$(CI_REGISTRY)/ska-telescope/$(NAME)/$(NAME):$(OCI_TAG)
+endif
+
+# https://github.com/pytest-dev/pytest-bdd/issues/401
+PYTHON_VARS_BEFORE_PYTEST = PYTHONDONTWRITEBYTECODE=True
+
+# better be verbose for debugging
+PYTHON_VARS_AFTER_PYTEST ?= -v
+
+# Add cucumber/json reports only for this test
+ifeq ($(MAKECMDGOALS),k8s-test)
+# execute in truel context; add BDD test results to be uploaded to xray
+PYTHON_VARS_AFTER_PYTEST += --true-context --cucumberjson=build/reports/cucumber.json \
+	--json-report --json-report-file=build/reports/report.json
+
+# hack out PYTHONPATH - why is it even there?
+# hack in test target directory
+K8S_TEST_TEST_COMMAND = unset PYTHONPATH; TANGO_HOST=$(TANGO_HOST) \
+						pytest \
+						$(PYTHON_VARS_AFTER_PYTEST) ./tests/functional \
+						 | tee pytest.stdout ## k8s-test test command to run in container
+endif
--- resources/Makefile_old.mk
+++ resources/Makefile_old.mk
@@ -3,17 +3,29 @@
 #########################################################################
 # This Makefile was copied from the Low ITF repository and caused a world
 # of pain. Don't just use.
+PROJECT = ska-ser-test-equipment
 
--include PrivateRules.mak
+# only publish main chart not test umbrella
+HELM_CHARTS_TO_PUBLISH=$(PROJECT)
 
-#inlucde basic necessities for the Makefiles to work
--include .make/base.mk
+# Chart for testing
+MINIKUBE ?= true
+VALUES ?=
+TANGO_HOST ?= tango-databaseds:10000  ## TANGO_HOST needed for k8s-test
 
-########################################################################
-# PYTHON
-########################################################################
+#### PYTHON CUSTOM VARS
+DOCS_SPHINXOPTS=-n -W --keep-going
 
--include .make/python.mk
+# Use the previously built image when running in the pipeline
+ifneq ($(CI_JOB_ID),)
+OCI_TAG = $(VERSION)-dev.c$(CI_COMMIT_SHORT_SHA)
+CI_REGISTRY ?= registry.gitlab.com
+# For k8s-install-chart
+VALUES += --set test_equipment.image.registry=$(CI_REGISTRY)/ska-telescope/$(NAME) \
+	--set test_equipment.image.tag=$(OCI_TAG)
+# for k8s-test
+K8S_TEST_IMAGE_TO_TEST=$(CI_REGISTRY)/ska-telescope/$(NAME)/$(NAME):$(OCI_TAG)
+endif
 
 # https://github.com/pytest-dev/pytest-bdd/issues/401
 PYTHON_VARS_BEFORE_PYTEST = PYTHONDONTWRITEBYTECODE=True
@@ -21,84 +33,43 @@ PYTHON_VARS_BEFORE_PYTEST = PYTHONDONTWRITEBYTECODE=True
 # better be verbose for debugging
 PYTHON_VARS_AFTER_PYTEST ?= -v
 
-PYTHON_TEST_FILE = tests/unit
+# Add cucumber/json reports only for this test
+ifeq ($(MAKECMDGOALS),k8s-test)
+# execute in truel context; add BDD test results to be uploaded to xray
+PYTHON_VARS_AFTER_PYTEST += --true-context --cucumberjson=build/reports/cucumber.json \
+	--json-report --json-report-file=build/reports/report.json
+
+# hack out PYTHONPATH - why is it even there?
+# hack in test target directory
+K8S_TEST_TEST_COMMAND = unset PYTHONPATH; TANGO_HOST=$(TANGO_HOST) \
+						pytest \
+						$(PYTHON_VARS_AFTER_PYTEST) ./tests/functional \
+						 | tee pytest.stdout ## k8s-test test command to run in container
+endif
+
+K8S_CHART = test-parent
+K8S_CHART_PARAMS = --set global.tango_host=$(TANGO_HOST) --set global.minikube=$(MINIKUBE) $(VALUES)
+
+-include PrivateRules.mak
+
+include .make/base.mk
+
+include .make/python.mk
+
+include .make/oci.mk
+
+include .make/helm.mk
+
+include .make/k8s.mk
+
+python-post-format:
+	docformatter -r -i --pre-summary-newline src/ tests/
 
 python-post-lint:
 	mypy --config-file mypy.ini src/ tests/
 
-.PHONY: python-post-lint
+### PYTHON END
 
-########################################################################
-# DOCS
-########################################################################
+k8s-reinstall-chart: k8s-uninstall-chart k8s-install-chart
 
--include .make/docs.mk
-
-DOCS_SPHINXOPTS = -n -W --keep-going
-
-########################################################################
-# OCI
-########################################################################
-
-include .make/oci.mk
-
-########################################################################
-# Helm
-########################################################################
-
-include .make/helm.mk
-
-# only publish main chart not test parent
-HELM_CHARTS_TO_PUBLISH=$(PROJECT_NAME)
-
-########################################################################
-# K8S
-########################################################################
-
-include .make/k8s.mk
-
-DEPLOYMENT_CONTEXT ?= ITF-mid # TODO: Ask @Malte what this means.
-
-# Chart for testing
-ifeq ($(shell kubectl config current-context),minikube)
-MINIKUBE ?= true
-else
-MINIKUBE ?= false
-endif
-
-ifneq ($(CI_JOB_ID),)
-# For k8s-install-chart
-_gitlab_image_tag = $(VERSION)-dev.c$(CI_COMMIT_SHORT_SHA)
-_gitlab_registry = $(CI_REGISTRY_IMAGE)
-else
-# If we're running locally, use the most recent image from GitLab
-_gitlab_image_tag = $(VERSION)-dev.c$(shell git rev-parse --short=8 @{u})
-_gitlab_registry = registry.gitlab.com/$(shell git remote -v | head -1 | grep -oE 'ska-telescope(/[a-z0-9-]{1,}){1,}')
-endif
-
-K8S_CHART_PARAMS += \
-	--set global.minikube=$(MINIKUBE) \
- 	# --set ska-psi-low-subrack.image.registry=$(_gitlab_registry) \
- 	# --set ska-psi-low-subrack.image.tag=$(_gitlab_image_tag)
-
-# hack out PYTHONPATH - why is it even there? #TODO: Ask at CoP what the issue is here.
-# hack in test target directory
-K8S_TEST_TEST_COMMAND = unset PYTHONPATH; \
-						$(PYTHON_VARS_BEFORE_PYTEST) pytest \
-						$(PYTHON_VARS_AFTER_PYTEST) ./tests/functional \
-						 | tee pytest.stdout ## k8s-test test command to run in container
-
-ifeq ($(MAKECMDGOALS),k8s-test)
-PYTHON_VARS_AFTER_PYTEST += \
-    --cucumberjson=build/reports/cucumber.json \
-	--json-report --json-report-file=build/reports/report.json
-
-K8S_CHART_PARAMS += --set ska-taranta.enabled=false
-endif
-
-K8S_TEST_RUNNER_ADD_ARGS += --env=TANGO_HOST=$(shell helm get -n $(KUBE_NAMESPACE) values -a $(HELM_RELEASE) -o json | jq '.global.tango_host')
-
-# PROXY_VALUES = \
-# --env=http_proxy=${http_proxy} \
-# --env=https_proxy=${http_proxy} \
-# --env=no_proxy=${no_proxy}
+.PHONY: python-post-format python-post-lint
