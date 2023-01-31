3156ae21c334238e7afb18c837732a30620bb377
Nkosingphile Nzotho <nnzotho@sarao.ac.za>
2023-01-31 14:27:19 +0200

    AT-331 Updated sky_simulator_controller.feature file and test_sky_simulator_controller.py

--- tests/functional/test_sky_simulator_controller.py
+++ tests/functional/test_sky_simulator_controller.py
@@ -1,64 +1,55 @@
-"""Sky Simulator Controller feature tests."""
+"""Sky Simulator components can be controlled remotely using Tango device server that communicates with Raspberry Pi using SCPI commands feature tests."""
 
-from typing import Optional
-
-import pytest
-from pytest_bdd import given, parsers, scenario, then, when
-
-
-@pytest.mark.xfail(
-    reason="BDD tests are required to fail, before the code implementation."
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
 )
-@scenario(
-    "features/sky_simulator_controller.feature",
-    "Test SkySimCtl can switch ON signal sources",
-)
-def test_skysimctl_can_switch_on_signal_sources():
+
+
+@scenario('features/sky_simulator_controller.feature', 'Test SCPI device returns identity')
+def test_test_scpi_device_returns_identity():
+    """Test SCPI device returns identity."""
+
+
+@scenario('features/sky_simulator_controller.feature', 'Test SkySimCtl can switch ON signal sources')
+def test_test_skysimctl_can_switch_on_signal_sources():
     """Test SkySimCtl can switch ON signal sources."""
 
 
-@given("the SkySimController is initialised (all signal sources OFF)")
-def skysim_controller_initialised():
-    """
-    Assert that the  SkySimController is initialised.
-
-    :raises NotImplementedError: because this command is not yet
-        implemented
-    """
-    raise NotImplementedError("Not part of the current story")
+@given('the SkySimController is initialised (all signal sources OFF)')
+def _():
+    """the SkySimController is initialised (all signal sources OFF)."""
+    raise NotImplementedError
 
 
-@given("the SkySimController is online")
-def skysim_controller_online():
-    """
-    Assert that the SkySimController is online.
-
-    :raises NotImplementedError: because this command is not yet
-        implemented
-    """
-    raise NotImplementedError("Not part of the current story")
+@given('the SkySimController is online')
+def _():
+    """the SkySimController is online."""
+    raise NotImplementedError
 
 
-@when(parsers.parse(("I switch on the <signal_source_name>")))
-def switch_on_signal_source(signal_source_name: Optional[dict] = None) -> None:
-    """
-    Assert that I switch on the <signal_source_name>.
-
-    :param signal_source_name: signal source name to be used
-        when the signal source needs to be switched on
-
-    :raises NotImplementedError: because this command is not yet
-        implemented
-    """
-    raise NotImplementedError("Not part of the current story")
+@when('I ask its identity')
+def _():
+    """I ask its identity."""
+    raise NotImplementedError
 
 
-@then("the <signal_source_name> must be ON")
-def signal_source_on():
-    """
-    Assert that the <signal_source_name> must be ON.
+@when('I switch on the <signal_source_name>')
+def _():
+    """I switch on the <signal_source_name>."""
+    raise NotImplementedError
 
-    :raises NotImplementedError: because this command is not yet
-        implemented
-    """
-    raise NotImplementedError("Not part of the current story")
+
+@then('it responds "SkySimController"')
+def _():
+    """it responds "SkySimController"."""
+    raise NotImplementedError
+
+
+@then('the <signal_source_name> must be ON')
+def _():
+    """the <signal_source_name> must be ON."""
+    raise NotImplementedError
+
