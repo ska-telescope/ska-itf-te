3099dce85334b1446814d3c8caba5565e7e0e709
Adriaan de Beer <adebeer42@gmail.com>
2023-02-07 11:53:33 +0200

    AT-331 BDD test now working correctly

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -2,7 +2,7 @@
 
 from pytest_bdd import given, parsers, scenario, then
 
-from ska_sky_simulator_controller import __name__ as name
+from ska_sky_simulator_controller import __version__ as version
 
 
 @scenario("features/version.feature", "Test version of skysimctl package")
@@ -13,14 +13,14 @@ def test_version_of_skysimctl_package():
 @given("an imported ska_sky_simulator_controller package")
 def imported_package():
     """Fails if there is no imported ska_sky_simulator_controller package."""
-    print(name)
+    print(version)
 
 
-@then(parsers.parse("its version is {version}"))
-def check_version(version):
+@then(parsers.parse("its version is {test_version}"))
+def check_version(test_version):
     """
     Package version is 0.1.0.
 
     :param version: the version of the imported package - see Jira issue.
     """
-    assert version == "0.1.0"
+    assert version == test_version
