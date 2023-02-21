5b92cf7a606968faed9d456bc3792c5797886e75
Adriaan de Beer <adebeer42@gmail.com>
2023-02-21 17:26:02 +0200

    AT-353 shuffled around a few charts

--- resources/k8s-installs.mk
+++ resources/k8s-installs.mk
@@ -1,12 +1,9 @@
 k8s-install-spookd:
 	make k8s-install-chart K8S_UMBRELLA_CHART_PATH=ska-mid-itf-spookd KUBE_APP=spookd KUBE_NAMESPACE=spookd
 
-# install taranta application
-k8s-install-taranta-application:
-#TODO: add target
-
 # install taranta dashboards in separate namespace
 k8s-install-taranta-dashboards:
 #TODO: add target
 
-#TODO: Lots of targets missing for different things
+#TODO: add target for skysimcontroller and other Test Equipment
+
