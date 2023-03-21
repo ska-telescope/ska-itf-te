fe700c9d31c43f22ef1082d877fbecd681c2fc00
Adriaan de Beer <adebeer42@gmail.com>
2023-03-21 20:50:18 +0200

    AT-423 added a first stab Gitlab pipeline job for deploying all the things

--- resources/k8s-installs.mk
+++ resources/k8s-installs.mk
@@ -8,4 +8,3 @@ k8s-install-taranta-dashboards:
 	@make k8s-install-chart K8S_UMBRELLA_CHART_PATH=ska-tango-base KUBE_APP=tango-base KUBE_NAMESPACE=tango-base
 	@make k8s-install-ska-tango-taranta-dashboard-pvc K8S_UMBRELLA_CHART_PATH=ska-tango-taranta-dashboard-pvc KUBE_APP=tango-base KUBE_NAMESPACE=tango-tar-pvc
 #TODO: add target for skysimcontroller and other Test Equipment
-
