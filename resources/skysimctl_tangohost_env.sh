c19592427425676b7ef558c33d33f4646a731533
Adriaan de Beer <adebeer42@gmail.com>
2023-06-23 17:44:54 +0200

    AT-532 use trigger jobs and dotenv reports to set up downstream environment

--- resources/skysimctl_tangohost_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This is a template for generating the environment for a Tango Device Server setup.
-# Copyright 2023 Adriaan de Beer, SARAO, under BSD License - see https://gitlab.com/ska-telescope/ska-mid-itf/-/blob/main/LICENSE
-
-# set the TANGO_HOST
