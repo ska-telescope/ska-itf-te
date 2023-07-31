0e5837c8747ec538bce1a5cf320a5f9ba55b551c
Adriaan de Beer <adebeer42@gmail.com>
2023-07-31 14:29:12 +0200

    AT-1018 cosmetic changes to the Values file and checks for changes specified more specifically for release candidate deployment

--- resources/k8s-installs.mk
+++ resources/k8s-installs.mk
@@ -1,15 +1,4 @@
-ifeq ($(CI_JOB_NAME),deploy-test-equipment-for-verification) # if CI_JOB_NAME is deploy-test-equipment
-# # Set K8S_EXTRA_PARAMS for deploying Test Equipment during development of the Test Equipment charts
-TE_REGISTRY ?= registry.gitlab.com/ska-telescope/ska-ser-test-equipment
-TE_IMAGE ?= ska-ser-test-equipment
-TE_VERSION ?= 0.7.4-dev.c92d86ef7 # This should be dynamically inherited and used only when the upstream changes
-
-K8S_EXTRA_PARAMS = \
-			--set test-equipment.image.registry=$(TE_REGISTRY) \
-			--set test-equipment.image.image=$(TE_IMAGE) \
-			--set test-equipment.image.tag=$(TE_VERSION) \
-			--set test-equipment.image.pullPolicy=Always
-endif
+include ./resources/test-equipment-dev.mk
 
 ## TARGET: itf-te-install
 ## SYNOPSIS: make itf-te-install
