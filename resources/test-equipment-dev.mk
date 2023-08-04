1626c84e58b8a3ae8e19f9c98f8845a9418274ce
Adriaan de Beer <adebeer42@gmail.com>
2023-08-04 12:23:22 +0200

    AT-1018 deploying Programmable Attenuator to verify it works in the ITF cluster

--- resources/test-equipment-dev.mk
+++ resources/test-equipment-dev.mk
@@ -6,7 +6,7 @@ ifeq ($(CI_JOB_NAME),deploy-test-equipment-for-verification) # if CI_JOB_NAME is
 # # Set K8S_EXTRA_PARAMS for deploying Test Equipment during development of the Test Equipment charts
 TE_REGISTRY ?= registry.gitlab.com/ska-telescope/ska-ser-test-equipment
 TE_IMAGE ?= ska-ser-test-equipment
-TE_VERSION ?= 0.7.4-dev.c92d86ef7 # This should be dynamically inherited and used only when the upstream changes
+TE_VERSION ?= 0.7.4-dev.cc164381c # This should be dynamically inherited and used only when the upstream changes
 
 K8S_EXTRA_PARAMS = \
 			--set test-equipment.image.registry=$(TE_REGISTRY) \
