6f1ec4192fe187983843ad514943d239c977febd
Johan Coetzer <johan.coetzer@gmail.com>
2023-08-21 11:15:24 +0200

    AT-1015 verify new version of spectrum analyser

--- resources/test-equipment-dev.mk
+++ resources/test-equipment-dev.mk
@@ -6,7 +6,8 @@ ifeq ($(CI_JOB_NAME),deploy-test-equipment-for-verification) # if CI_JOB_NAME is
 # # Set K8S_EXTRA_PARAMS for deploying Test Equipment during development of the Test Equipment charts
 TE_REGISTRY ?= registry.gitlab.com/ska-telescope/ska-ser-test-equipment
 TE_IMAGE ?= ska-ser-test-equipment
-COMMIT_HASH ?= 8bb6ae37
+# COMMIT_HASH ?= 8bb6ae37
+COMMIT_HASH ?= 55a9d1e0
 TE_VERSION ?= 0.8.3 # this line can be commented out or overwritten by the following line
 # TE_VERSION ?= 0.8.0-dev.c$(COMMIT_HASH) # This is the version of the image that we want to pull from https://gitlab.com/ska-telescope/ska-ser-test-equipment/container_registry/3213235
 
