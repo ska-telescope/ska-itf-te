37363c67516d64653758b7ce24e2ad89fc3581f4
nnzotho <nnzotho@sarao.ac.za>
2023-10-11 19:30:31 +0200

    AT-1480 Testing ODA deploy

--- resources/makefiles/k8s-installs.mk
+++ resources/makefiles/k8s-installs.mk
@@ -58,10 +58,10 @@ itf-spookd-template-chart:
 
 ODA_NAMESPACE=oda
 itf-install-oda:
-	make k8s-install-chart K8S_CHART=ska-db-oda KUBE_APP=oda KUBE_NAMESPACE=$(ODA_NAMESPACE)
+	make k8s-install-chart K8S_CHART=ska-db-oda KUBE_APP=oda KUBE_NAMESPACE=$(ODA_NAMESPACE) HELM_RELEASE=installing-oda
 
 itf-uninstall-oda:
-	make k8s-uninstall-chart K8S_CHART=ska-db-oda KUBE_APP=oda KUBE_NAMESPACE=$(ODA_NAMESPACE)
+	@make k8s-uninstall-chart K8S_CHART=ska-db-oda KUBE_APP=oda KUBE_NAMESPACE=$(ODA_NAMESPACE) HELM_RELEASE=installing-oda
 
 # install taranta dashboards in separate namespace
 k8s-install-taranta-dashboards:
