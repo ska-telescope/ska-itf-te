18f8a189c7dd2b58134c1d27ddc1554267c526e7
nnzotho <nnzotho@sarao.ac.za>
2023-10-27 00:42:41 +0200

    AT-1216 Updating tango db and registering SPFC to network.

--- scripts/spfc/register_device.py
+++ scripts/spfc/register_device.py
@@ -1,22 +1,43 @@
+import os
 import subprocess
 import sys
 import tango
-from tango import Database, DbDevInfo
+from time import sleep
+from tango import Database, DbDevInfo,DeviceProxy
+import threading
 
+def register_device():
+        msg_processing_thread = threading.Thread(target=self.process_send_msg_queue, daemon=True)
+        msg_processing_thread.start()
 def main():
     dev_info1 = DbDevInfo()
-    print(tango.ApiUtil.get_env_var("TANGO_HOST"))
-    dev_info1.name = 'SPFRx/rxpu/controller'
-    dev_info1._class = 'SPFRx'
-    dev_info1.server = 'SPFRx/test'
-    try:
-        dish_lmc_ip_address = sys.argv[1]
-        database = Database(str(dish_lmc_ip_address), 10000)
-        database.add_server(dev_info1.server, dev_info1, with_dserver=True)
-        dev_info = database.get_device_info(dev_info1.name)
-        print(f"Registered device is {str(dev_info)} ")
-    except IndexError:
-        print(f"Error: Please call  \' {str(sys.argv) } \' with ip address argument.")
+    tango_db_env = str(tango.ApiUtil.get_env_var("TANGO_HOST"))
+    print(tango_db_env)
+    tango_db_ip = str(tango_db_env.split(":")[0])
+    tango_db_port = int(tango_db_env.split(":")[1])
+    dev_info1.name = sys.argv[1]
+    dev_info1._class = sys.argv[2]
+    dev_info1.server = sys.argv[3]
+    database = Database(tango_db_ip, tango_db_port)
+    exception = True
+    int_success_cause = 60
+    while (True):
+        try:
+            database.add_device(dev_info1)
+            sleep(2)
+            dp = DeviceProxy(dev_info1.name)
+            print(dp.get_attribute_list())
+        except Exception as ex:
+            exception = True
+            print("Exception occured:")
+            print(ex)
+            continue
+        else:
+            print("Registration succeded.")
+            print(f"Now will pause for { int_success_cause } seconds")
+            sleep(int_success_cause)
+            exception = False
+        sleep(2)
 
 if __name__ == "__main__":
     main()
