f9a6b5430881ec0f71cd209618fa357c36c09f70
nnzotho <nnzotho@sarao.ac.za>
2023-11-05 13:25:03 +0200

    AT-1216 Updated register_device.py to add properties once the device has been registered.

--- scripts/spfc/register_device.py
+++ scripts/spfc/register_device.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import subprocess
 import sys
@@ -5,52 +6,91 @@ import tango
 from time import sleep
 from tango import Database, DbDevInfo,DeviceProxy
 import threading
-import logging as log_test
+class RegisterSPFC:
+    __database = None
+    __dev_info = None
+    __dev_proxy = None
+    __server_location = ""
+    __serial_number = ""
+    __dict_device_names = dict([ ("spfc", "Spfc"), ("spf1", "Spf1"), ("spf2", "Spf2"),
+                                 ("spf345", "Spf345"), ("spfhe","SpfHe"), ("spfvac","SpfVac")])
+        
+    def register_all_devices(self, server_location, serial_number):
+        self.__server_location = server_location
+        tango_db_env = str(tango.ApiUtil.get_env_var("TANGO_HOST"))
+        print(tango_db_env)
+        tango_db_ip = str(tango_db_env.split(":")[0])
+        tango_db_port = int(tango_db_env.split(":")[1])
+        self.__database = Database(tango_db_ip, tango_db_port)
+        self.__dev_info = DbDevInfo()
 
-def register_all_devices():
-    tango_db_env = str(tango.ApiUtil.get_env_var("TANGO_HOST"))
-    print(tango_db_env)
-    tango_db_ip = str(tango_db_env.split(":")[0])
-    tango_db_port = int(tango_db_env.split(":")[1])
-    database = Database(tango_db_ip, tango_db_port)
-    dev_info = DbDevInfo()
-    logname = "register_spfc_logs.log"
+        for (dev_name,class_name) in self.__dict_device_names.items():
+            dev_class = str(dev_name[0].upper()) + str(dev_name[1:])
+            self.__dev_info.name = self.__server_location + "/spf/" + dev_name
+            self.__dev_info._class = class_name
+            self.__dev_info.server = class_name + "/" + self.__serial_number            
+            msg_processing_thread = threading.Thread(target=self.register_device)
+            msg_processing_thread.start()
+            sleep(2)
 
-    log_test.basicConfig(filename=logname,
-                        filemode='a',
-                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                        datefmt='%H:%M:%S',
-                        level=log_test.DEBUG)
+    def add_device_properties(self, dev_name):
+        match dev_name:
+            #spfc
+            case list(self.__dict_device_names.keys())[0]:
+                ls_properties = {"SpfHeLocation":[self.__server_location+"/spf/spfhe"], "SpfVacLocation":[self.__server_location+"/spf/spfvac"],
+                                "Spf1Location":[self.__server_location+"/spf/spf1"], "Spf2Location":[self.__server_location+"/spf/spf2"],
+                                "Spf345Location":[self.__server_location+"/spf/spf345"]}
+            #spfc1
+            case list(self.__dict_device_names.keys())[1]:
+                ls_properties = {"ttyPort":["/dev/ttyS1"]}
+            #spfc2
+            case list(self.__dict_device_names.keys())[2]:
+                ls_properties = {"ttyPort":["/dev/ttyS2"], "SpfHeLocation":[self.__server_location+"/spf/spfhe"],
+                                "SpfVacLocation":[self.__server_location+"/spf/spfvac"]}
+            #spfc345
+            case list(self.__dict_device_names.keys())[3]:
+                ls_properties = {"ttyPort":["/dev/ttyS3"], "SpfHeLocation":[self.__server_location+"/spf/spfhe"], 
+                                 "SpfVacLocation":[self.__server_location+"/spf/spfvac"]}
 
-    ls_device_names = ["spfc", "spf1", "spf2", "spf345", "spfhe", "spfvac"]
-    for dev_name in ls_device_names:
-        dev_class = str(dev_name[0].upper()) + str(dev_name[1:])
-        dev_info.name = "ska000/spf/" + dev_name 
-        dev_info._class = dev_class
-        dev_info.server = dev_class + "/4F1018"      
-        register_device(dev_info, database)
-        sleep(2)
+        print(f"Adding {dev_name}'s propeties...")
+        self.__database.put_device_property(self.__dev_info.name, ls_properties)
+
+
+    def register_device(self):
+        exception = True
+        int_success_pause = 60
+        while (exception):
+            try:
+                self.__database.add_device(self.__dev_info)
+                sleep(1)
+                self.__dev_proxy = DeviceProxy(self.__dev_info.name)
+                print(f"server========={self.__dev_info.server}==========")
+                print(f"class={self.__dev_info._class}")
+                print(f"server={self.__dev_info.server}\n\n")
+                print(self.__dev_proxy.get_attribute_list())
+            except Exception as ex:
+                exception = True
+                print(f"!!Reg. {self.__dev_info.name} \n Exception occured:")
+                print(ex)
+                sleep(2)
+                continue
+            else:
+                exception = False
+            sleep(5)
+        #Devices are registered now we add their properties
+        print("Adding device properties...")
+        for (dev_name, class_name) in self.__dict_device_names.items():
+            self.add_device_properties(dev_name)
+            sleep(5)
 
 def main():
-    register_all_devices()
-
-def register_device(dev_info, database):
-    exception = True
-    int_success_pause = 60
-    while (exception):
-        sleep(1)
-        try:
-            database.add_device(dev_info)
-            dp = DeviceProxy(dev_info.name)
-            print(dp.get_attribute_list())
-        except Exception as ex:
-            exception = True
-            print(f"!!Registering name={dev_info.name} \n Exception occured!!")
-            print(ex)
-            pass
-        else:
-            log_test.info(f"== {dev_info.name} registered ==")
-            exception = False
+    reg_spfc = RegisterSPFC
+    pars = argparse.ArgumentParser()
+    pars.add_argument("dev_location", help="Device server location, for example ska001", type=str)
+    pars.add_argument("serial_number", help="SPFC serial number, for exmaple 4F0001 (found in /var/lib/spfc/spfc/spfc_config.ini within SPFC device)", type=str)
+    device_location = pars.parse_args().dev_location
+    serial_number = pars.parse_args().serial_number
+    reg_spfc.register_all_devices(device_location, serial_number)
 
 if __name__ == "__main__":
     main()
