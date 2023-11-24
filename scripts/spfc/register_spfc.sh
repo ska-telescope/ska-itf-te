1831af2837a04d9793ca7f23448277050a70ce6b
nnzotho <nnzotho@sarao.ac.za>
2023-11-24 13:32:01 +0200

    AT-1216 Updated scripts and charts for testing

--- scripts/spfc/register_spfc.sh
+++ scripts/spfc/register_spfc.sh
@@ -1,4 +1,2 @@
- #!/bin/bash
-
- python3 -c "import requests;[r = requests.get('http://localhost:8080/', headers={'Accept': 'application/json'}) print(f"Response: {r.json()}")]"
- 
+#!/bin/bash
+printenv
