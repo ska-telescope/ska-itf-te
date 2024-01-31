a7878cd8d48615928be201208f7f02bce5632e81
shraddhabajare <shraddhab.ska@gmail.com>
2024-01-31 22:14:29 +0530

    SAH-1486: Add fixture for responce data

--- tests/integration/alarmhandler/test_add_alarms_api.py
+++ tests/integration/alarmhandler/test_add_alarms_api.py
@@ -11,6 +11,24 @@ namespace = os.getenv("KUBE_NAMESPACE")
 logger = logging.getLogger(__name__)
 
 
+class ResponseData(object):
+    """Class to have response data received."""
+
+    def __init__(self) -> None:
+        """Initiaise class variables."""
+        self.response = None
+
+
+@pytest.fixture(name="response_data")
+def fixture_default_response():
+    """Set up default responce.
+
+    :return: A class object representing default response for the system under test.
+    """
+    response_data = ResponseData()
+    return response_data
+
+
 @pytest.mark.skamid
 @scenario("features/add_alarms.feature", "Configure TMC Alarms")
 def test_tmc_mid_configure_alarms():
@@ -30,9 +48,10 @@ def test_tmc_mid_configure_alarms():
         "I configure alarms with {alarm_rule_file} for TMC using alarm configurator tool"
     )
 )
-def add_alarms_api(context, alarm_rule_file):
+def add_alarms_api(response_data, alarm_rule_file):
     """Call add-alarms API.
 
+    :param response_data: fixture for responce data
     :param alarm_rule_file: alarm rules file as input for add-alarms API
     """
     file_path = os.path.join(
@@ -40,25 +59,25 @@ def add_alarms_api(context, alarm_rule_file):
     )
     logging.info(file_path)
     with open(file_path, "rb") as file:
-        response = httpx.post(
+        add_api_response = httpx.post(
             f"http://alarm-handler-configurator.{namespace}.svc.miditf.internal.skao.int"
             + ":8004/add-alarms?fqdn=alarm%2Fhandler%2F01",
             files={"file": (alarm_rule_file, file, "text/plain")},
             data={"fqdn": "alarm/handler/01"},
         )
-        logging.info(response)
-        context.response_data = response.json()
-        logging.info(context.response_data)
+        logging.info(add_api_response)
+        response_data.response = add_api_response.json()
+        logging.info(response_data.response)
 
 
 @then("TMC alarms are configured successfully")
-def check_alarms(context):
+def check_alarms(response_data):
     """Check add-alarms API response.
 
-    :param response_data: json received from add-alarms API
+    :param response_data: fixture for responce data
     """
-    assert len(context.response_data["alarm_summary"]["tag"]) == 1
-    assert context.response_data["alarm_summary"]["tag"] == [
+    assert len(response_data.response["alarm_summary"]["tag"]) == 1
+    assert response_data.response["alarm_summary"]["tag"] == [
         "subarraynode_obsstate_fault",
     ]
 
