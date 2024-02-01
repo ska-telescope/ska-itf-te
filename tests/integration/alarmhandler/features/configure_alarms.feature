13e19adac951bb6e9240c6842c465325203f155c
shraddhabajare <shraddhab.ska@gmail.com>
2024-02-01 23:00:56 +0530

    SAH-1486: Modify test scenario

--- tests/integration/alarmhandler/features/configure_alarms.feature
+++ tests/integration/alarmhandler/features/configure_alarms.feature
@@ -1,9 +1,9 @@
-Scenario: Configure Alarm for STANDBY State
+Scenario: Configure Alarm for UNKNOWN State
     Given a mid telescope
     Given an alarm handler
     Given an alarm handler is configured to raise an alarm when the <device_name> State <state_value>
-    When telescope remains in STANDBY state for long
+    When telescope remains in UNKNOWN state for long
     Then alarm must be raised with UNACKNOWLEDGE state
     Examples:
         |device_name | state_value |
-        |CentralNode | STANDBY     |
+        |CentralNode | UNKNOWN     |
--- tests/integration/alarmhandler/test_alarm_configure.py
+++ tests/integration/alarmhandler/test_alarm_configure.py
@@ -5,10 +5,9 @@ import os
 import httpx
 import pytest
 
-# from assertpy import assert_that
+from assertpy import assert_that
 from pytest_bdd import given, parsers, scenario, then, when
 from ska_ser_skallop.connectors import configuration as con_config
-from ska_ser_skallop.event_handling.builders import get_message_board_builder
 from ska_ser_skallop.mvp_control.describing import mvp_names as names
 
 # from ska_ser_skallop.mvp_fixtures.fixtures import fxt_types
@@ -20,8 +19,8 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.skamid
-@scenario("features/configure_alarms.feature", "Configure Alarm for STANDBY State")
-def test_tmc_mid_standby_state_alarm():
+@scenario("features/configure_alarms.feature", "Configure Alarm for UNKNOWN State")
+def test_tmc_alarm_for_state_unknown():
     """Configure and raise alarms."""
 
 
@@ -40,21 +39,21 @@ def test_tmc_mid_standby_state_alarm():
     )
 )
 def configure_alarm_state(response_data, device_name, state_value):
-    """Alarm is configured for STANDBY state.
+    """Alarm is configured for UNKNOWN state.
 
     :param response_data: fixture for response data
     :param device_name: tango device for alarm condition
     :param state_value: tango device attribute value for alarm condition
     """
     file_path = os.path.join(
-        os.getcwd(), "tests/integration/alarmhandler/data/alarm_rules/standby_alarm_rule.txt"
+        os.getcwd(), "tests/integration/alarmhandler/data/alarm_rules/alarm_rule_state_unknown.txt"
     )
     logging.info(file_path)
     with open(file_path, "rb") as file:
         add_api_response = httpx.post(
             f"http://alarm-handler-configurator.{namespace}.svc.miditf.internal.skao.int"
             + ":8004/add-alarms?fqdn=alarm%2Fhandler%2F01",
-            files={"file": ("standby_alarm_rule.txt", file, "text/plain")},
+            files={"file": ("alarm_rule_state_unknown.txt", file, "text/plain")},
             data={"fqdn": "alarm/handler/01"},
         )
         logging.info(add_api_response)
@@ -66,30 +65,13 @@ def configure_alarm_state(response_data, device_name, state_value):
         ]
 
 
-@when("telescope remains in STANDBY state for long")
+@when("telescope remains in UNKNOWN state for long")
 def check_alarms():
-    """Check telescope in STANDBY."""
+    """Check telescope in UNKNOWN."""
     tel = names.TEL()
     central_node = con_config.get_device_proxy(tel.tm.central_node)
-    central_node.command_inout("TelescopeOn")
-    brd = get_message_board_builder()
-    # context_monitoring.wait_for(central_node).for_attribute("telescopeState").to_become_equal_to(
-    #     "ON", ignore_first=False, settings=integration_test_exec_settings
-    # )
-    brd.set_waiting_on(tel.tm.central_node).for_attribute("telescopeState").to_become_equal_to(
-        "ON"
-    )
-    on_result = central_node.read_attribute("telescopeState").value
-    logging.info(on_result)
-    central_node.command_inout("TelescopeStandby")
-    # context_monitoring.wait_for(central_node).for_attribute("telescopeState").to_become_equal_to(
-    #     "STANDBY", ignore_first=False, settings=integration_test_exec_settings
-    # )
-    brd.set_waiting_on(tel.tm.central_node).for_attribute("telescopeState").to_become_equal_to(
-        "STANDBY"
-    )
-    standby_result = central_node.read_attribute("telescopeState").value
-    logging.info(standby_result)
+    result = central_node.read_attribute("telescopeState").value
+    assert_that(str(result)).is_equal_to("UNKNOWN")
 
 
 @then("alarm must be raised with UNACKNOWLEDGE state")
@@ -99,13 +81,10 @@ def check_alarm_state(response_data):
     :param response_data: fixture for response data
     """
     logging.info(response_data.response)
-    brd = get_message_board_builder()
-    brd.set_waiting_on("alarm/handler/01").for_attribute("alarmUnacknowledged").to_become_equal_to(
-        ("centralnode_telescopestate_standby",)
-    )
     alarm_handler = DeviceProxy("alarm/handler/01")
-    ah_result = alarm_handler.read_attribute("alarmUnacknowledged").value
+    ah_result = alarm_handler.read_attribute("alarmUnacknowledged")
     logging.info(ah_result)
     assert response_data.response["alarm_summary"]["state"] == [
         "UNACK",
     ]
+    alarm_handler.Ack(response_data.response["alarm_summary"]["tag"])
