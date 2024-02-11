59b5f7a885045a4c3c2158fca0454727331c1b93
Adriaan de Beer <adebeer42@gmail.com>
2024-02-12 00:16:57 +0200

    AT-1838 updated steps

--- steps.sh
+++ steps.sh
@@ -1,5 +1,5 @@
 # run the dockerfile
-docker run -it --env-file PrivateRules.mak registry.gitlab.com/ska-telescope/ska-mid-itf-engineering-tools/ska-mid-itf-engineering-tools:0.1.4
+docker run -it --env-file PrivateRules.mak registry.gitlab.com/ska-telescope/ska-mid-itf-engineering-tools/ska-mid-itf-engineering-tools:0.1.5
 
 # git clone (to mimic the pipeline start)
 mkdir /build && mkdir /build/ska-telescope && cd /build/ska-telescope 
