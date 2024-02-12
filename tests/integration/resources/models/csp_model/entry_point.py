261a4e48eaa8987536b393eb3f398bb10a702d55
Pieter Jordaan <gitlab@jordaan.me>
2024-02-12 15:10:21 +0200

    AT-1828 - Back to manual dish parameters.

--- tests/integration/resources/models/csp_model/entry_point.py
+++ tests/integration/resources/models/csp_model/entry_point.py
@@ -83,27 +83,33 @@ class StartUpStep(base.StartUpStep, LogEnabled, WithCommandID):
         :raises CommandException: when the command returned as failed
         """
         assert self.long_running_command_subscriber
+        #dish_cfg = {
+        #    "interface": "https://schema.skao.int/ska-mid-cbf-initsysparam/1.0",
+        #    "tm_data_sources": ["car://gitlab.com/ska-telescope/ska-telmodel-data?ska-sdp-tmlite-repository-1.0.0#tmdata"], 
+        #    "tm_data_filepath": "instrument/ska1_mid_itf/ska-mid-cbf-system-parameters.json",
+        #}
         dish_cfg = {
             "interface": "https://schema.skao.int/ska-mid-cbf-initsysparam/1.0",
             "dish_parameters": {
                 "SKA001": {
                     "vcc": 1,
-                    "k": 11,
+                    "k"  : 111,
                 },
                 "SKA036": {
                     "vcc": 2,
-                    "k": 101,
+                    "k"  : 222,
                 },
                 "SKA063": {
                     "vcc": 3,
-                    "k": 1127,
+                    "k"  : 333,
                 },
                 "SKA100": {
                     "vcc": 4,
-                    "k": 620,
+                    "k"  : 444,
                 },
             },
         }
+
         load_cfg_id = self.csp_controller.command_inout("loadDishCfg", json.dumps(dish_cfg))
         if not command_success(load_cfg_id):
             self.long_running_command_subscriber.unsubscribe_all()
