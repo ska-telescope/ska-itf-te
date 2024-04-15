4ef826215271bc5e08bc02a2f5c0f0fb7c1df2b4
Adriaan de Beer <adebeer42@gmail.com>
2024-04-15 20:15:08 +0200

    AT-1981 added pydantic-core

--- steps.sh
+++ steps.sh
@@ -43,3 +43,9 @@ make itf-cbf-config-mcs && sleep 3
 make itf-cbf-tangocpp-update &> deploy/talonconfig.log && sleep 3
 make itf-cbf-config-tangodb && sleep 3
 make itf-cbf-tango-on || cat /app/src/ska_mid_itf_engineering_tools/cbf_config/talon_on.py && make itf-cbf-power-on && echo "###############\n# Failed to deploy Talon Demonstrator Correlator\n###############" > deploy/status
+
+# Run integration test
+export TANGO_DATABASE_DS=tango-databaseds
+export KUBE_NAMESPACE=integration
+export CLUSTER_DOMAIN=miditf.internal.skao.int
+make integration-test TANGO_HOST=$TANGO_DATABASE_DS.$KUBE_NAMESPACE.svc.$CLUSTER_DOMAIN:10000
