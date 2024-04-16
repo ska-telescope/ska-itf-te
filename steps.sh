d80f81a59237b7e186825d83b4c420f94551c9c9
Adriaan de Beer <adebeer42@gmail.com>
2024-04-16 12:46:47 +0200

    AT-1981 better steps

--- steps.sh
+++ steps.sh
@@ -3,7 +3,9 @@
 #############################################################################################################################
 
 # run the dockerfile
-docker run -it -e CI_COMMIT_SHA=$(git rev-parse --short HEAD) --env-file PrivateRules.mak registry.gitlab.com/ska-telescope/ska-mid-itf-engineering-tools/ska-mid-itf-engineering-tools:0.7.1
+IMAGE=registry.gitlab.com/ska-telescope/ska-mid-itf-engineering-tools/ska-mid-itf-engineering-tools
+IMAGE_VERSION=0.7.1
+docker run -it -e CI_COMMIT_SHA=$(git rev-parse --short HEAD) --env-file PrivateRules.mak $IMAGE:$IMAGE_VERSION
 
 
 # git clone (to mimic the pipeline start)
