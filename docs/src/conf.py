c8c13535b7194f0dfd68f860a8cc1de3ba770600
Adriaan de Beer <adebeer42@gmail.com>
2024-04-18 22:43:58 +0200

    AT-1981 bumped patch

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = '21.0.5'
+release = '21.0.6'
 
 # -- General configuration ---------------------------------------------------
 
