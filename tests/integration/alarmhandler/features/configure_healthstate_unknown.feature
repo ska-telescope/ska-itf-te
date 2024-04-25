6212ddb58ff58124ee1b9c0a59c66cb956d7e88e
shraddhabajare <shraddhab.ska@gmail.com>
2024-04-25 19:42:54 +0530

    AT-2026: Modify alarm tests accordingly to updated k8s-test deployment.

--- tests/integration/alarmhandler/features/configure_healthstate_unknown.feature
+++ tests/integration/alarmhandler/features/configure_healthstate_unknown.feature
@@ -5,9 +5,8 @@ Feature: SP-3833
 	Scenario: Configure alarm rule for healthState UNKNOWN
 		Given a mid telescope
 		Given an alarm handler
-		Given an alarm handler is configured to raise an alarm when the <device1> <device2> healthState 
-		When <device1> and <device2> remain in healthState UNKNOWN for long
-		Then alarm for healthState UNKNOWN must be raised with UNACKNOWLEDGE state
+		When an alarm handler is configured to raise an alarm when the <device1> <device2> healthState 
+		Then Alarms are configured succesfully for <device1> and <device1>
 		Examples:
 		    |         device1                | device2                        | 
 		    |ska_mid/tm_central/central_node | ska_mid/tm_subarray_node/1     |
--- tests/integration/alarmhandler/test_alarm_for_unknown_health_state.py
+++ tests/integration/alarmhandler/test_alarm_for_unknown_health_state.py
@@ -39,7 +39,7 @@ def test_tmc_alarm_for_healthstate_unknown():
 # use @given("an alarm handler") from ..conftest
 
 
-@given(
+@when(
     parsers.parse(
         "an alarm handler is configured to raise an "
         + "alarm when the {device1} {device2} healthState"
@@ -64,42 +64,16 @@ def configure_alarm_healthstate(response_data, device1, device2):
             data={"trl": "alarm/handler/01"},
         )
         response_data.response = add_api_response.json()
-        assert len(response_data.response["alarm_summary"]["tag"]) == 1
-        assert f"{device1}" in response_data.response["alarm_summary"]["formula"][0]
-        assert f"{device2}" in response_data.response["alarm_summary"]["formula"][0]
 
 
-# This test case is based on real scenario when some devices are not present
-# in this case its Dish Masters.
-# when all devices are available this scenario might not be the same.
-@when(parsers.parse("{device1} and {device2} remain in healthState UNKNOWN for long"))
-def check_alarms(device1, device2):
-    """Check devices in healthState UNKNOWN.
+@then(parsers.parse("Alarms are configured succesfully for {device1} and {device1}"))
+def check_alarms(response_data, device1, device2):
+    """Check alarms are configured
+    for the devices.
 
     :param device1: tango device1 with healthState UNKNOWN
     :param device2: tango device2 with healthState UNKNOWN
     """
-    tango_device1 = con_config.get_device_proxy(device1)
-    tango_device2 = con_config.get_device_proxy(device2)
-    device1_result = tango_device1.read_attribute("telescopehealthState").value
-    device2_result = tango_device2.read_attribute("healthState").value
-    # If the dish is deployed the value will not be UNKNOWN
-    assert_that(device1_result).is_equal_to(HealthState.UNKNOWN)
-    assert_that(device2_result).is_equal_to(HealthState.UNKNOWN)
-
-
-@then("alarm for healthState UNKNOWN must be raised with UNACKNOWLEDGE state")
-def check_alarm_state(response_data):
-    """Check alarm state.
-
-    :param response_data: fixture for response data
-    """
-    alarm_handler = DeviceProxy("alarm/handler/01")
-    alarm_tag = response_data.response["alarm_summary"]["tag"]
-    brd = get_message_board_builder()
-    # If the dish is deployed the alarm will not be raised
-    brd.set_waiting_on("alarm/handler/01").for_attribute("alarmUnacknowledged").to_become_equal_to(
-        alarm_tag,
-    )
-    # acknowledge the alarm
-    alarm_handler.Ack(alarm_tag)
+    assert len(response_data.response["alarm_summary"]["tag"]) == 1
+    assert f"{device1}" in response_data.response["alarm_summary"]["formula"][0]
+    assert f"{device2}" in response_data.response["alarm_summary"]["formula"][0]
