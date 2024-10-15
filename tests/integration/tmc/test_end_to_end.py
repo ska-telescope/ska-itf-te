dcc2b71dc06047ab46a65c56207d96716d8d7159
Akhil-AJ <joseph_ajj@outlook.com>
2024-10-15 17:32:16 +0000

    MAP-166 Update e2e test to accomodate new CBF behaviour

--- tests/integration/tmc/test_end_to_end.py
+++ tests/integration/tmc/test_end_to_end.py
@@ -139,6 +139,19 @@ def _(telescope_handlers):
 
     assert csp_control.ping() > 0
 
+    if (csp_control.adminMode == 0) or (csp_subarray.adminMode == 0):
+        # CSP should be OFFLINE when CBF Sim mode is set
+        csp_control.adminMode = 1
+        csp_subarray.adminMode = 1
+        wait_for_event(csp_control, "adminMode", 0)
+        wait_for_event(csp_subarray, "adminMode", 0)
+        sleep(2)
+
+    CBF_HW_IN_THE_LOOP = os.getenv("CBF_HW_IN_THE_LOOP", "false").lower()
+    if CBF_HW_IN_THE_LOOP in ["false", "0"]:
+        csp.set_cbf_simulation_mode(True)
+        sleep(2)
+
     csp_control.adminMode = 0
     csp_subarray.adminMode = 0
     wait_for_event(csp_control, "adminMode", 0)
