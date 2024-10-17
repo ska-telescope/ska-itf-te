934498697418684ebadb601beb50e1e3595682f5
Akhil-AJ <joseph_ajj@outlook.com>
2024-10-17 10:20:21 +0000

    MAP-166 Remove adminMode events

--- tests/integration/tmc/test_end_to_end.py
+++ tests/integration/tmc/test_end_to_end.py
@@ -143,23 +143,23 @@ def _(telescope_handlers):
         # CSP should be OFFLINE when CBF Sim mode is set
         csp_control.adminMode = 1
         csp_subarray.adminMode = 1
-        wait_for_event(csp_control, "adminMode", 1)
-        wait_for_event(csp_subarray, "adminMode", 1)
-        sleep(2)
+        # wait_for_event(csp_control, "adminMode", 1)
+        # wait_for_event(csp_subarray, "adminMode", 1)
+        sleep(4)
 
     CBF_HW_IN_THE_LOOP = os.getenv("CBF_HW_IN_THE_LOOP", "false").lower()
     if CBF_HW_IN_THE_LOOP in ["false", "0"]:
         csp.set_cbf_simulation_mode(True)
-        sleep(2)
+        sleep(4)
 
     csp_control.adminMode = 0
     csp_subarray.adminMode = 0
-    wait_for_event(csp_control, "adminMode", 0)
-    wait_for_event(csp_subarray, "adminMode", 0)
-    sleep(5)  # TODO: Find out exactly why this is needed
-    csp_control.Off("")  # TODO: Find out exactly why this is needed
-    csp_subarray.Off()  # TODO: Find out exactly why this is needed
-    sleep(5)  # TODO: Find out exactly why this is needed
+    # wait_for_event(csp_control, "adminMode", 0)
+    # wait_for_event(csp_subarray, "adminMode", 0)
+    # sleep(5)  # TODO: Find out exactly why this is needed
+    # csp_control.Off("")  # TODO: Find out exactly why this is needed
+    # csp_subarray.Off()  # TODO: Find out exactly why this is needed
+    # sleep(5)  # TODO: Find out exactly why this is needed
 
 
 @when("I turn ON the telescope")
