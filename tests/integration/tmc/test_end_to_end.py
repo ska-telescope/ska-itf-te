7be8171f1b9daa69604d8fef739a70de4dc26dbb
Akhil-AJ <joseph_ajj@outlook.com>
2024-10-17 11:28:23 +0000

    MAP-166 Turn Off CSP before loadDishCfg

--- tests/integration/tmc/test_end_to_end.py
+++ tests/integration/tmc/test_end_to_end.py
@@ -154,16 +154,15 @@ def _(telescope_handlers):
 
     csp_control.adminMode = 0
     csp_subarray.adminMode = 0
-    sleep(5)
     logger.info(
         f"CSP adminMode is: {csp_control.adminMode}, CBF Simulation mode is: {csp_control.cbfSimulationMode}"
     )
     # wait_for_event(csp_control, "adminMode", 0)
     # wait_for_event(csp_subarray, "adminMode", 0)
-    # sleep(5)  # TODO: Find out exactly why this is needed
-    # csp_control.Off("")  # TODO: Find out exactly why this is needed
-    # csp_subarray.Off()  # TODO: Find out exactly why this is needed
-    # sleep(5)  # TODO: Find out exactly why this is needed
+    sleep(5)  # TODO: Find out exactly why this is needed
+    csp_control.Off("")  # TODO: Find out exactly why this is needed
+    csp_subarray.Off()  # TODO: Find out exactly why this is needed
+    sleep(5)  # TODO: Find out exactly why this is needed
 
 
 @when("I turn ON the telescope")
