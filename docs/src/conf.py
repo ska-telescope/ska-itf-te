cf7fc1342116fa6d90b3c324c135d33a5a346b8a
Adriaan de Beer <adebeer42@gmail.com>
2024-11-15 15:42:55 +0200

    REL-1832 New Release: Four Receptors

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "24.5.0rc5"
+release = "24.5.0"
 
 # -- General configuration ---------------------------------------------------
 
