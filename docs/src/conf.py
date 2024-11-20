ba85b79e6f80e5bdf00376498d7792cecf66bb2f
Adriaan de Beer <adebeer42@gmail.com>
2024-11-20 14:37:10 +0200

    AT-2526 Bump RC version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "24.5.1rc2"
+release = "24.5.1rc3"
 
 # -- General configuration ---------------------------------------------------
 
