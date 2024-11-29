3a0648b457cf4a3becc86efbc09bddffd19ee001
Akhil-AJ <joseph_ajj@outlook.com>
2024-11-29 14:46:36 +0000

    MAP-212 Bump rc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "24.5.1rc8"
+release = "24.5.1rc9"
 
 # -- General configuration ---------------------------------------------------
 
