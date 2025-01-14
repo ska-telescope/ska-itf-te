44f6fe78612e4bb67a9f90771a8cec4e478ccda3
Adriaan de Beer <adebeer42@gmail.com>
2025-01-14 15:10:22 +0200

    REL-1894 bumped RC version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "25.3.0rc2"
+release = "25.3.0rc3"
 
 # -- General configuration ---------------------------------------------------
 
