0d1d2c0fa55c4a944733c6266a5cb2072af7a525
Akhil-AJ <joseph_ajj@outlook.com>
2025-01-22 15:57:41 +0000

    AT-2582 Bump rc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "25.3.0"
+release = "25.3.1"
 
 # -- General configuration ---------------------------------------------------
 
