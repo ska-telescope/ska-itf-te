264a96eecf0a209b69d627a83658ce7793538145
David-mh <dhorn@sarao.ac.za>
2025-01-29 09:20:31 +0200

    AT-2660 roll back MCS and EC

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "25.3.2"
+release = "25.3.3rc1"
 
 # -- General configuration ---------------------------------------------------
 
