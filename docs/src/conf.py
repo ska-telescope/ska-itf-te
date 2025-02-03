95e1fdef693cd733211b312ccab603984680c3cd
Adriaan de Beer <adebeer42@gmail.com>
2025-02-03 16:52:27 +0200

    REL-1941 bumped release version & minor fix to RTD file

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "25.4.0rc3"
+release = "25.4.0"
 
 # -- General configuration ---------------------------------------------------
 
