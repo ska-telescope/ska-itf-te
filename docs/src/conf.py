7598032e4cc195b6ff5c7f459a677e9576be046c
Adriaan de Beer <adebeer42@gmail.com>
2025-02-11 11:40:28 +0000

    AT-2702 bump patch

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "25.4.1"
+release = "25.4.2"
 
 # -- General configuration ---------------------------------------------------
 
