218549c3e571a28a100fe6434b365bc45080b750
Akhil-AJ <joseph_ajj@outlook.com>
2025-02-11 13:34:02 +0000

    AT-2703 Add latency calc

--- utils/track_events/subscriber.py
+++ utils/track_events/subscriber.py
@@ -1,6 +1,7 @@
 """ Light Client"""
 import logging
 from os import getenv
+from datetime import datetime
 import time
 import tango
 logger = logging.getLogger("event-subscriber")
@@ -8,6 +9,17 @@ logger = logging.getLogger("event-subscriber")
 def callback(event):
     """callback"""
     logger.info(f"{event}")
+    print(f"{event}")
+    pub_time = timeval_to_datetime(event.attr_value.time)
+    reception_time = timeval_to_datetime(event.reception_date)
+    print(f"Latency: {reception_time - pub_time}")
+
+def timeval_to_datetime(time_val):
+    sec = time_val.tv_sec
+    nsec = time_val.tv_nsec
+    usec = time_val.tv_usec
+    time_datetime = datetime.fromtimestamp(sec+usec/1e6+nsec/1e9)
+    return time_datetime
 
 if __name__ == "__main__":
     attr_name = getenv("ATTR_NAME")
