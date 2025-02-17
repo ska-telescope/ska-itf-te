669ca8f013144d73912d043244ef976ad613ddca
Adriaan de Beer <adebeer42@gmail.com>
2025-02-17 22:28:19 +0200

    AT-2685 RC version bump

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "25.6.0rc1"
+release = "25.6.0rc2"
 
 # -- General configuration ---------------------------------------------------
 
