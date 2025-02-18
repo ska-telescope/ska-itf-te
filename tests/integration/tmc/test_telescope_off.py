9cd9c5922cb048619c71a0818b82c7465642ecd9
Akhil-AJ <joseph_ajj@outlook.com>
2025-02-18 23:41:12 +0000

    AT-2647 Add seperate off test

---
+++ tests/integration/tmc/test_telescope_off.py
@@ -0,0 +1,20 @@
+"""Telescope OFF test feature tests."""
+
+from pytest_bdd import scenario, then
+from tango import DevState
+
+
+@scenario("features/test_telescope_off.feature", "Telescope OFF via TMC")
+def test_telescope_off_via_tmc():
+    """Telescope ON via TMC."""
+
+
+@then("the telescope is in the OFF state")
+def _(telescope_handlers):
+    """Telescope is in the OFF state.
+
+    :param telescope_handlers: _description_
+    :type telescope_handlers: _type_
+    """
+    tmc, _, _, _ = telescope_handlers
+    assert tmc.central_node.telescopeState == DevState.OFF
