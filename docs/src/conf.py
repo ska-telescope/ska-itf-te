81cce5e9620b60c8ba35cf504ef76a3605f805da
Adriaan de Beer <adebeer42@gmail.com>
2025-02-18 16:10:17 +0200

    AT-2726 bumped Minor version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "25.4.0"
+release = "25.5.0"
 
 # -- General configuration ---------------------------------------------------
 
