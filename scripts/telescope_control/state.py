972d72fb04f446e8cf06d7412e11777207ecb48e
Akhil-AJ <joseph_ajj@outlook.com>
2025-02-20 21:08:53 +0000

    AT-2647 Fix base state configuration

--- scripts/telescope_control/state.py
+++ scripts/telescope_control/state.py
@@ -3,7 +3,8 @@ import argparse
 import logging
 
 from utils.telescope_teardown import TelescopeHandler, TelescopeState
-from tango import DevState
+from ska_control_model._dev_state import DevState
+from ska_control_model import ObsState
 from utils.enums import DishMode
 
 logger = logging.getLogger()
@@ -13,6 +14,7 @@ parser = argparse.ArgumentParser(
     prog="Telescope control", description="Telescope monitoring and manipulation", epilog=""
 )
 parser.add_argument("-p", "--print-state", action="store_true", help="Print telescope state")
+parser.add_argument("--print-base-state", action="store_true", help="Print telescope state")
 parser.add_argument("-n", "--namespace", help="Specify namespace to execute against")
 parser.add_argument(
     "-d", "--dish-ids", help="Provide dish ID's as a space seperated string e.g. 'SKA001 SKA036'"
@@ -21,10 +23,13 @@ parser.add_argument(
     "-t", "--teardown", action="store_true", help="Teardown the telescope to base state"
 )
 parser.add_argument(
-    "-b", "--subarray-base-state", help="Provide the base subarray node state e.g. ON"
+    "-c", "--central-node-base-state", help="Provide the base subarray node state e.g. READY"
 )
 parser.add_argument(
-    "-s", "--dish-base-state", help="Provide the base dish state e.g. STANDBY_LP"
+    "-s", "--subarray-base-state", help="Provide the base subarray node state e.g. READY"
+)
+parser.add_argument(
+    "-b", "--dish-base-state", help="Provide the base dish state e.g. STANDBY_FP"
 )
 args = parser.parse_args()
 
@@ -35,33 +40,36 @@ def main(args=args):
     :param args: _description_, defaults to args
     :type args: _type_, optional
     """
-    subarray_base_state = "ON"
-    dish_base_state = "STANDBY_LP"
     
     if args.namespace and args.dish_ids:
         dish_ids = args.dish_ids.split(" ")
         telescope = TelescopeHandler(args.namespace, dish_ids)
+        initial_dish_base_state = {dish_id: DishMode.STANDBY_LP for dish_id in dish_ids}
+        base_state = TelescopeState(dishes=initial_dish_base_state)
+
+        if args.central_node_base_state:
+            state_name = args.central_node_base_state.upper()
+            base_state.central_node = DevState[state_name]
+            logger.info("Set cn base state")
 
         if args.subarray_base_state:
-            if args.subarray_base_state.lower() == "on":
-                subarray_base_state = DevState.ON
-                telescope_base_state = TelescopeState(telescope=subarray_base_state)
-                telescope.telescope_base_state = telescope_base_state
-            else:
-                #TODO
-                pass
+            state_name = args.subarray_base_state.upper()
+            base_state.subarray = ObsState[state_name]
+            logger.info("Set subarray base state")
                 
         if args.dish_base_state:
-            if args.dish_base_state.lower() == "standby_fp":
-                dish_base_state = {dish_id: DishMode.STANDBY_FP for dish_id in dish_ids}
-                telescope_base_state = TelescopeState(telescope=subarray_base_state, dishes=dish_base_state)
-                telescope.telescope_base_state = telescope_base_state
-            else:
-                # TODO
-                pass
+            state_name = args.dish_base_state.upper()
+            dish_base_state = {dish_id: DishMode[state_name] for dish_id in dish_ids}
+            base_state.dishes = dish_base_state
+            logger.info("Set dish base state")
+
+        telescope.telescope_base_state = base_state
                 
         if args.print_state:
             logger.info(f"Current telescope state: {telescope.get_current_state()}")
+
+        if args.print_base_state:
+            logger.info(f"Current base state: {telescope.telescope_base_state}")
 
         if args.teardown:
             telescope.teardown()
