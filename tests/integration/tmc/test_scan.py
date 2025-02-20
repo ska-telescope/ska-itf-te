624b27ddc43f4ddda899732fd3da4cf768501f6c
Akhil-AJ <joseph_ajj@outlook.com>
2025-02-20 23:12:15 +0000

    AT-2647 Mark HW focused tests

--- tests/integration/tmc/test_scan.py
+++ tests/integration/tmc/test_scan.py
@@ -2,7 +2,7 @@
 
 from pytest_bdd import scenario
 
-
+@pytest.mark.hw_in_the_loop
 @scenario("features/test_scan.feature", "Perform a scan via TMC")
 def test_perform_a_scan_via_tmc():
     """Perform a scan via TMC."""
