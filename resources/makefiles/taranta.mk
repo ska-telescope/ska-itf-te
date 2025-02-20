ad7602b36a5734d502412620bee7d005caed5afd
Adriaan de Beer <adebeer42@gmail.com>
2025-02-20 21:59:25 +0200

    AT-2584 run MultiDB deployment target in pipeline

--- resources/makefiles/taranta.mk
+++ resources/makefiles/taranta.mk
@@ -6,7 +6,7 @@
 ##
 ##  Create a TangoGQL deployment instance
 
-taranta-deploy-dish-tangogql: TARANTA_PARAMS="--set tangogql.tangoDB=$(DISH_ID) --set tangogql.tango_host=$(TANGO_DATABASE_DS).$(KUBE_NAMESPACE)-dish-lmc-$(DISH_ID).svc.$(CLUSTER_DOMAIN):10000"
+taranta-deploy-dish-tangogql: TARANTA_PARAMS="--set tangogql.tangoDB=$(DISH_ID) --set global.tango_host=$(TANGO_DATABASE_DS).$(KUBE_NAMESPACE)-dish-lmc-$(DISH_ID).svc.$(CLUSTER_DOMAIN):10000 --set global.cluster_domain=$(CLUSTER_DOMAIN)"
 
 taranta-deploy-dish-tangogql: taranta-check-env ## Deploy TangoGQL instance
 	$(info Deploying one instance of TangoGQL in a namespace with one existing tangoGQL instance already running)
@@ -15,7 +15,7 @@ taranta-deploy-dish-tangogql: taranta-check-env ## Deploy TangoGQL instance
 	$(info Helm Release for this deployment: $(HELM_RELEASE)-$(DISH_ID))
 	$(info Tango Host: $(TANGO_DATABASE_DS).$(KUBE_NAMESPACE)-dish-lmc-$(DISH_ID).svc.$(CLUSTER_DOMAIN):10000)
 	$(info Setting Helm Chart Params: $(TARANTA_PARAMS))
-	@make k8s-install-chart K8S_CHART=taranta-itf K8S_CHART_PARAMS=$(TARANTA_PARAMS) HELM_RELEASE=$(HELM_RELEASE)-$(DISH_ID)
+	@make k8s-template-chart K8S_CHART=taranta-itf K8S_CHART_PARAMS=$(TARANTA_PARAMS) HELM_RELEASE=$(HELM_RELEASE)-$(DISH_ID)
 
 .PHONY: taranta-check-env
 
