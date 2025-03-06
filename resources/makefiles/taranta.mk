c03bde8c2a7f453665ed64e54f2c989059243fb5
Adriaan de Beer <adebeer42@gmail.com>
2025-03-06 12:16:15 +0100

    AT-2802 fixing namespace postfix for tangoGQL

--- resources/makefiles/taranta.mk
+++ resources/makefiles/taranta.mk
@@ -6,7 +6,7 @@
 ##
 ##  Create a TangoGQL deployment instance
 
-taranta-deploy-dish-tangogql: TARANTA_PARAMS="--set tangogql.tangoDB=$(DISH_ID) --set global.tango_host=$(TANGO_DATABASE_DS).$(KUBE_NAMESPACE)-dish-lmc-$(DISH_ID).svc.$(CLUSTER_DOMAIN):10000 --set global.cluster_domain=$(CLUSTER_DOMAIN)"
+taranta-deploy-dish-tangogql: TARANTA_PARAMS="--set tangogql.tangoDB=$(DISH_ID) --set global.tango_host=$(TANGO_DATABASE_DS).$(DISH_NAMESPACE).svc.$(CLUSTER_DOMAIN):10000 --set global.cluster_domain=$(CLUSTER_DOMAIN)"
 
 taranta-deploy-dish-tangogql: taranta-check-env ## Deploy TangoGQL instance
 	$(info Deploying one instance of TangoGQL in a namespace with one existing tangoGQL instance already running)
@@ -23,7 +23,17 @@ LOWER_DISH_IDS=$(shell echo $(DISH_IDS) | tr A-Z a-z)
 taranta-deploy-all-tangogql-instances:
 	$(info DISH_IDS: $(LOWER_DISH_IDS))
 	for ID in $(LOWER_DISH_IDS); do \
-	make taranta-deploy-dish-tangogql DISH_ID=$$ID; \
+		if [[ ! -z "$(CI_COMMIT_TAG)" ]]; then \
+			DISH_NAMESPACE=staging-dish-lmc-$$ID; \
+			make taranta-deploy-dish-tangogql DISH_ID=$$ID DISH_NAMESPACE=staging-dish-lmc-$$ID; \
+		elif [[ "$(CI_COMMIT_BRANCH)" != "$(CI_DEFAULT_BRANCH)" ]]; then \
+			DISH_NAMESPACE=dish-lmc-$$ID-$(CI_COMMIT_BRANCH); \
+			make taranta-deploy-dish-tangogql DISH_ID=$$ID DISH_NAMESPACE=dish-lmc-$$ID-$(CI_COMMIT_BRANCH); \
+		else \
+			DISH_NAMESPACE=integration-dish-lmc-$$ID; \
+			make taranta-deploy-dish-tangogql DISH_ID=$$ID DISH_NAMESPACE=integration-dish-lmc-$$ID; \
+		fi; \
+		$(info DISH_NAMESPACE: $(DISH_NAMESPACE)) \
 	done
 
 taranta-check-env: ## Private target: Check environment configuration variables
