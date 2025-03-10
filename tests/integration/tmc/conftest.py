d282c13291e991f3069eb77dcb459df85c0f59fe
Adriaan de Beer <adebeer42@gmail.com>
2025-03-10 09:58:05 +0200

    AT-2766 applied MR review suggestions

--- tests/integration/tmc/conftest.py
+++ tests/integration/tmc/conftest.py
@@ -295,7 +295,7 @@ def receptor_ids():
     :return: List of receptor IDs
     :rtype: _type_
     """
-    receptors = ["SKA001", "SKA036", "SKA063", "SKA100"]  # TEMP COMMIT
+    receptors = ["SKA001", "SKA036", "SKA063", "SKA100"]
     return receptors
 
 
