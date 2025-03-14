3550598d8e7297c3e88a3bd9068fbe0df22f6480
David-mh <dhorn@sarao.ac.za>
2025-03-14 14:29:56 +0200

    AT-2831 rc release changes

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "26.0.0rc1"
+release = "26.0.0rc2"
 
 # -- General configuration ---------------------------------------------------
 
