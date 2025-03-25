c8f5c8d470babf24db55550e26c917e3fe929fd9
David-mh <dhorn@sarao.ac.za>
2025-03-25 17:00:19 +0200

    AT-2831 tagged rc release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "26.0.0rc3"
+release = "26.0.0rc7"
 
 # -- General configuration ---------------------------------------------------
 
