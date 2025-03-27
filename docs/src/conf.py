85dc10b059f13292b8df1c4e90164d7785fcfb1e
David-mh <dhorn@sarao.ac.za>
2025-03-27 08:57:54 +0200

    AT-2831 updates to assign resources in submodule

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "26.0.1rc1"
+release = "26.0.1rc2"
 
 # -- General configuration ---------------------------------------------------
 
