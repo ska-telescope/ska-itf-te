ce51bb59d66f7b31f18aa55b697486a64c7bfb4d
Johan Coetzer <johan.coetzer@tsolo.io>
2025-04-04 14:31:44 +0200

    AT-2885 add option to write empty secrets where applicable

--- scripts/mid_aiv_vault.py
+++ scripts/mid_aiv_vault.py
@@ -13,9 +13,11 @@ LOG_LEVEL = logging.WARNING
 logging.basicConfig(level=LOG_LEVEL)
 _module_logger = logging.getLogger(__name__)
 
+
+
 DEFAULT_URL = "http://127.0.0.1:8200"
-MID_AIV_MOUNTS = ["cubbyhole/", "dev/", "mid-aa/", "mid-itf/", "shared/"]
-DATA_PATH = "tmp/vault"
+DEFAULT_SECRET_ENGINES = ["cubbyhole/", "dev/", "mid-aa/", "mid-itf/", "shared/"]
+DEFAULT_DATA_PATH = "tmp/vault"
 DATA_FORMATS = ["yaml", "json"]
 DEFAULT_DATA_FORMAT = "yaml"
 
@@ -97,20 +99,22 @@ def list_secrets():
 
 
 def read_secret_values(
-    secret_path: str,
+    secret_data_path: str,
     mval: str,
     secret_data: str,
     data_format: str,
+    create_empty: bool,
 ) -> int:
     """
     Read secret values.
 
-    :param secret_path: where to write secrets
+    :param secret_data_path: where to write secrets
     :param data_format: format, i.e. yaml or json
+    :param create_empty: create empty directories and files
     :returns: error code
     """
-    _module_logger.debug("Secret values path %s: %s", secret_path, mval)
-    values_path = os.path.join(secret_path, mval)
+    _module_logger.debug("Secret values path %s: %s", secret_data_path, mval)
+    values_path = os.path.join(secret_data_path, mval)
     try:
         os.makedirs(values_path)
     except FileExistsError:
@@ -123,12 +127,20 @@ def read_secret_values(
         values_dict = json.loads(secret_data)
     else:
         _module_logger.error("Format %s not supported", data_format)
-        return
+        return 1
     _module_logger.debug("%s", json.dumps(values_dict, indent=4))
     try:
         values_yml = values_dict["data"]["data"]["values.yml"]
+    except TypeError:
+        values_yml = ""
     except KeyError:
-        _module_logger.error("No values found")
+        values_yml = ""
+    if not values_yml:
+        _module_logger.warning("No values found")
+        if create_empty:
+            with open(values_file, "w") as foo:
+                os.utime(values_file, None)
+            return 0
         return 1
     _module_logger.debug(values_yml)
     with open(values_file, "w") as foo:
@@ -136,17 +148,20 @@ def read_secret_values(
     return 0
 
 
-def read_secret(secret_path: str, mval: str, data_format: str) -> int:
+def read_secret(
+    secret_data_path: str, mval: str, data_format: str, create_empty: bool
+) -> int:
     """
     Read secret.
 
-    :param secret_path: where to write secrets
+    :param secret_data_path: where to write secrets
     :param mval: value to read
     :param data_format: format, i.e. yaml or json
+    :param create_empty: create empty directories and files
     :returns: error code
     """
-    _module_logger.debug("Secret path %s: %s", secret_path, mval)
-    data_file = os.path.join(secret_path, f"{mval}.{data_format}")
+    _module_logger.debug("Secret path %s: %s", secret_data_path, mval)
+    data_file = os.path.join(secret_data_path, f"{mval}.{data_format}")
     data_path = os.path.dirname(data_file)
     _module_logger.debug("Create path %s", data_path)
     try:
@@ -163,56 +178,63 @@ def read_secret(secret_path: str, mval: str, data_format: str) -> int:
         )
     except subprocess.CalledProcessError:
         _module_logger.error("Could not get secret %s", mval)
-        # Create empty file
-        with open(data_file, "w") as foo:
-            os.utime(data_file, None)
+        if create_empty:
+            # Create empty file
+            with open(data_file, "w") as foo:
+                os.utime(data_file, None)
+            return 0
         return 1
     secret_data = kv_out.stdout
     _module_logger.debug("%s", secret_data)
     with open(data_file, "w") as foo:
         foo.write(secret_data)
-    read_secret_values(secret_path, mval, secret_data, data_format)
+    read_secret_values(secret_data_path, mval, secret_data, data_format, create_empty)
     return 0
 
 
-def read_secrets(
-    secret_path: str,
-    mid_aiv_paths: list,
-    mid_aiv_secrets: list,
-    vault_paths: list,
+def read_secret_engines(
+    secret_data_path: str,
+    secret_engines: list,
     data_format: str,
+    create_empty: bool,
 ) -> int:
     """
-    Read secrets.
+    Read secret engines.
 
-    :param secret_path: where to write secrets
-    :param mid_aiv_paths: Vault paths
-    :param mid_aiv_secrets: initial list of secrets
-    :param vault_paths: initial list of paths
+    :param secret_data_path: where to write secrets
+    :param secret_engines: initial list of paths
     :param data_format: format, i.e. yaml or json
+    :param create_empty: create empty directories and files
     :returns: error code
     """
-    _module_logger.debug("Secrets path %s", secret_path)
+    mid_aiv_paths: list = []
+    mid_aiv_secrets: list = []
+    _module_logger.debug("Secrets path %s", secret_data_path)
 
-    for vault_path in vault_paths:
-        if vault_path[-1:] != "/":
-            vault_path += "/"
-        kvs = kv_list(vault_path)
+    for secret_engine in secret_engines:
+        if secret_engine[-1:] != "/":
+            secret_engine += "/"
+        kvs = kv_list(secret_data_path, secret_engine, create_empty)
         for kv in kvs:
             if not kv:
-                pass
+                if create_empty:
+                    emptydir = os.path.join(secret_data_path, secret_engine)
+                    try:
+                        os.makedirs(emptydir)
+                    except FileExistsError:
+                        pass
             elif kv[-1:] == "/":
                 if kv not in mid_aiv_paths:
                     _module_logger.debug("Add path %s", kv)
-                    mid_aiv_paths.append(f"{vault_path}{kv}")
+                    mid_aiv_paths.append(f"{secret_engine}{kv}")
             else:
                 if kv not in mid_aiv_secrets:
-                    mid_aiv_secrets.append(f"{vault_path}{kv}")
+                    mid_aiv_secrets.append(f"{secret_engine}{kv}")
 
     while True:
         added = 0
         for mpath in mid_aiv_paths:
-            kvs = kv_list(mpath)
+            kvs = kv_list(secret_data_path, mpath, create_empty)
             for kv in kvs:
                 if not kv:
                     pass
@@ -236,23 +258,31 @@ def read_secrets(
     #     print(f"\t{mpath}")
     # print()
     for mval in sorted(mid_aiv_secrets):
-        read_secret(secret_path, mval, data_format)
+        read_secret(secret_data_path, mval, data_format, create_empty)
 
     return 0
 
 
-def kv_list(vault_path: str) -> list:
+def kv_list(secret_data_path: str, vault_path: str, create_empty: bool) -> list:
     """
     Read path.
 
+    :param secret_data_path: where to write secrets
     :param vault_path: vault path
+    :param create_empty: create empty directories and files
     :returns: list of paths found
     """
     _module_logger.debug("List path %s", vault_path)
     try:
         kv_out = subprocess.run(["vault", "kv", "list", vault_path], stdout=subprocess.PIPE, text=True, check=True)
     except subprocess.CalledProcessError:
-        _module_logger.error("Could not list path %s", vault_path)
+        _module_logger.warning("Could not list path %s", vault_path)
+        if create_empty:
+            emptydir = os.path.join(secret_data_path, vault_path)
+            try:
+                os.makedirs(emptydir)
+            except FileExistsError:
+                pass
         return []
     kvs = kv_out.stdout.split("\n")[2:]
     _module_logger.info("Path %s: %s", vault_path, ",".join(kvs))
@@ -272,20 +302,23 @@ def main(y_arg: list) -> int:
     :param y_arg: list of arguments
     :returns: error code
     """
-    mid_aiv_paths = []
-    mid_aiv_secrets = []
-    vault_host ="https://vault.skao.int/"
-    vault_path = ""
-    vault_paths = []
-    vault_token = ""
-    data_format = ""
+    secrets_data_path: str = ""
+    vault_host: str = "https://vault.skao.int/"
+    secret_engines: list = []
+    vault_token: str = ""
+    data_format: str = ""
+    create_empty: bool = False
 
+    # Read command line
     try:
         opts, _args = getopt.getopt(
             y_arg[1:],
-            "hvVF:H:P:T:",
+            "ehvVD:E:F:H:P:T:",
             [
                 "help",
+                "empty",
+                "data=",
+                "engines=",
                 "format=",
                 "host=",
                 "path=",
@@ -297,10 +330,15 @@ def main(y_arg: list) -> int:
         _module_logger.error("Could not read command line: %s", opt_err)
         return 1
 
+    # Read options
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             usage(os.path.basename(y_arg[0]))
             sys.exit(1)
+        elif opt in ("-D", "--data"):
+            secrets_data_path = arg
+        elif opt in ("-e", "--empty"):
+            create_empty = True
         elif opt in ("-F", "--format"):
             data_format = arg.lower()
             if data_format not in DATA_FORMATS:
@@ -310,9 +348,9 @@ def main(y_arg: list) -> int:
             vault_host = arg
         elif opt in ("-P", "--path"):
             if "," in arg:
-                vault_paths = arg.split(",")
+                secret_engines = arg.split(",")
             else:
-                vault_paths = [arg]
+                secret_engines = [arg]
         elif opt in ("-T", "--token"):
             vault_token = arg
         elif opt == "-v":
@@ -324,19 +362,28 @@ def main(y_arg: list) -> int:
         else:
             _module_logger.error("Invalid option %s", opt)
 
+    # Set up vault client
     _module_logger.info("Start client for %s", vault_host)
     client = hvac.Client(url=vault_host)
-
     if not client.is_authenticated():
         _module_logger.error(f"Client for host {vault_host} is not authenticated")
         return 1
 
-    if not vault_paths:
-        vault_paths = MID_AIV_MOUNTS
+    # Check values and use defaults as needed
+    if not secrets_data_path:
+        secrets_data_path = DEFAULT_DATA_PATH
+    try:
+        os.makedirs(secrets_data_path)
+    except FileExistsError:
+        pass
+    if not secret_engines:
+        secret_engines = DEFAULT_SECRET_ENGINES
     if not data_format:
         data_format = DEFAULT_DATA_FORMAT
 
-    read_secrets(DATA_PATH, mid_aiv_paths, mid_aiv_secrets, vault_paths, data_format)
+    read_secret_engines(secrets_data_path, secret_engines, data_format, create_empty)
+
+    # client.delete()
 
     return 0
 
