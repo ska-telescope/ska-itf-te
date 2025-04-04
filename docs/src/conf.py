7bde4a96f7b02a613ba2368833e183eaad7bc465
David-mh <dhorn@sarao.ac.za>
2025-04-04 10:27:44 +0200

    AT-2906 bump-minor-release updates

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -28,7 +28,7 @@ copyright = "Adriaan de Beer, adebeer@sarao.ac.za"
 author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
-release = "26.0.0"
+release = "26.1.0"
 
 # -- General configuration ---------------------------------------------------
 
