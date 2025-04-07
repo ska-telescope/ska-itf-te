758037bfbe4b9c44223fa69d8bf0e8650b4b3675
Akhil-AJ <joseph_ajj@outlook.com>
2025-04-07 15:40:28 +0000

    AT-2906 Bump rc, include new mcs and ec

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.1.0rc3"
+release = "26.1.0rc5"
 
 # -- General configuration ---------------------------------------------------
 
