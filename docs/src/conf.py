24a040ad39b6a2486b04577e6a7e91f5afb88d57
David-mh <dhorn@sarao.ac.za>
2025-04-08 17:02:01 +0200

    AT-2883 bump release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.1.1rc1"
+release = "26.1.1rc2"
 
 # -- General configuration ---------------------------------------------------
 
