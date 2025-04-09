03566ec1c5f7c70b3b7ecdc2caf296b41b305041
Akhil-AJ <joseph_ajj@outlook.com>
2025-04-09 14:25:11 +0000

    REL-2058 Release 26.1.2

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.1.1"
+release = "26.1.2"
 
 # -- General configuration ---------------------------------------------------
 
