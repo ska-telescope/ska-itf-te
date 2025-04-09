c291c06469421d65fd5fe20af36be93e6e7a3f80
David-mh <dhorn@sarao.ac.za>
2025-04-09 09:08:55 +0200

    AT-2883 - updted rc release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.1.1rc4"
+release = "26.1.1rc5"
 
 # -- General configuration ---------------------------------------------------
 
