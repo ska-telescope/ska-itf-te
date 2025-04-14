c4a4db01548c0a3436a9bb9f62df566ecce8ce19
Akhil-AJ <joseph_ajj@outlook.com>
2025-04-14 15:09:47 +0000

    AT-2938 Basic smoke test

---
+++ tests/smoke/test_smoke.py
@@ -0,0 +1,16 @@
+"""."""
+
+from tango import DeviceProxy
+
+
+def test_devices_reachable():
+    """Tests connectivity to tango devices.
+
+    This is a simple smoke test to check if the required devices are reachable.
+    It creates device proxies to various tango devices and checks if they are reachable.
+    """
+    device = DeviceProxy(
+        "tango-databaseds.staging.svc.miditf.internal.skao.int:10000/mid-tmc/central-node/0"
+    )
+
+    assert device.ping(), "Device is not reachable"
