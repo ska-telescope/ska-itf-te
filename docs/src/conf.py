53e11001eed4c61320fcc8e213bd0b7bd6e79a01
nnzotho <nnzotho@sarao.ac.za>
2025-04-15 22:38:59 +0200

    AT-2874: bumped version to 26.1.3-rc.2

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.1.3rc1"
+release = "26.1.3rc2"
 
 # -- General configuration ---------------------------------------------------
 
