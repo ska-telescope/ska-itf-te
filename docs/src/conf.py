2444028de8504171f0a18eee67ada513ceaaa257
AshleyVivo <ashley@vivosa.co.za>
2025-04-17 19:11:44 +0200

    AT-2874 bumped patch to separate from Dave's  work

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.1.3-rc.14"
+release = "26.1.4"
 
 # -- General configuration ---------------------------------------------------
 
