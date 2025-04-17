cdad82d2b077c041492e5ffec3bb451217e2dbdf
David-mh <dhorn@sarao.ac.za>
2025-04-17 10:34:39 +0200

    AT-2953 updated rc release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.1.3rc10"
+release = "26.1.3rc11"
 
 # -- General configuration ---------------------------------------------------
 
