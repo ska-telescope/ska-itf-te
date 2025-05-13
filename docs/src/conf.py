b618169e898bf44ea17161a8b15ea62604f3f009
David-mh <dhorn@sarao.ac.za>
2025-05-13 07:33:30 +0200

    AT-3054 updated bump release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.4.0rc9"
+release = "26.4.0rc10"
 
 # -- General configuration ---------------------------------------------------
 
