7f0921f99ea7897d07d549c314ab1d9a428448eb
AshleyVivo <ashley@vivosa.co.za>
2025-05-19 00:37:22 +0200

    AT-3031 fix comparing logic

--- scripts/kubernetes/compare_deployed_images.sh
+++ scripts/kubernetes/compare_deployed_images.sh
@@ -27,17 +27,28 @@ for k in "${!expected_versions[@]}"; do
   echo "  $k -> ${expected_versions[$k]}"
 done
 
-# Compare deployed images
+# Compare deployed to expected
 while IFS=":" read -r name deployed_image || [[ -n "$name" ]]; do
+  name="${name//\"/}"
+  name="${name## }"
+  name="${name%% }"
+
+  deployed_image="${deployed_image//\"/}"
+  deployed_image="${deployed_image## }"
+  deployed_image="${deployed_image%% }"
+
+  if [[ -z "$name" || -z "$deployed_image" ]]; then
+    continue
+  fi
+
   expected_image="${expected_versions[$name]:-}"
 
   if [[ -z "$expected_image" ]]; then
     echo "[WARN] $name is deployed with $deployed_image, but no expected image defined"
-    continue
-  fi
 
-  if [[ "$expected_image" != "$deployed_image" ]]; then
+  elif [[ "$expected_image" != "$deployed_image" ]]; then
     echo "[WARN] Mismatch in '$name': expected '$expected_image', found '$deployed_image'"
+
   else
     echo "[INFO] $name matches expected image version"
   fi
