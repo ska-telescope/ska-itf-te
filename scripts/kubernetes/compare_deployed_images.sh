4770d32d4377e5d1de5795ebfe0717064d1caaa5
AshleyVivo <ashley@vivosa.co.za>
2025-05-19 07:45:10 +0200

    AT-3031 overhaul script

--- scripts/kubernetes/compare_deployed_images.sh
+++ scripts/kubernetes/compare_deployed_images.sh
@@ -5,51 +5,44 @@ expected_file="$1"
 deployed_file="$2"
 
 declare -A expected_versions
+declare -A deployed_versions
 
-# Build map of expected images
-while IFS=":" read -r name image; do
-  # Sanitise both name and image
-  name="${name//\"/}"   # remove quotes
-  name="${name## }"     # trim leading space
-  name="${name%% }"     # trim trailing space
-
-  image="${image//\"/}"
-  image="${image## }"
-  image="${image%% }"
-
-  if [[ -n "$name" && -n "$image" ]]; then
-    expected_versions["$name"]="$image"
-  fi
+# Load expected
+while IFS=":" read -r name image_tag || [[ -n "$name" ]]; do
+  name="${name//\"/}"; name="${name## }"; name="${name%% }"
+  image_tag="${image_tag//\"/}"; image_tag="${image_tag## }"; image_tag="${image_tag%% }"
+  [[ -n "$name" && -n "$image_tag" ]] && expected_versions["$name"]="$image_tag"
 done < "$expected_file"
 
-echo "[DEBUG] Loaded expected images:"
-for k in "${!expected_versions[@]}"; do
-  echo "  $k -> ${expected_versions[$k]}"
-done
+# Load deployed
+while IFS=":" read -r name image_tag || [[ -n "$name" ]]; do
+  name="${name//\"/}"; name="${name## }"; name="${name%% }"
+  image_tag="${image_tag//\"/}"; image_tag="${image_tag## }"; image_tag="${image_tag%% }"
+  [[ -n "$name" && -n "$image_tag" ]] && deployed_versions["$name"]="$image_tag"
+done < "$deployed_file"
 
-# Compare deployed to expected
-while IFS=":" read -r name deployed_image || [[ -n "$name" ]]; do
-  name="${name//\"/}"
-  name="${name## }"
-  name="${name%% }"
+echo "[DEBUG] Checking deployed vs expected container versions..."
 
-  deployed_image="${deployed_image//\"/}"
-  deployed_image="${deployed_image## }"
-  deployed_image="${deployed_image%% }"
+# Check all expected containers
+for name in "${!expected_versions[@]}"; do
+  expected_image="${expected_versions[$name]}"
+  deployed_image="${deployed_versions[$name]:-}"
 
-  if [[ -z "$name" || -z "$deployed_image" ]]; then
+  if [[ -z "$deployed_image" ]]; then
+    echo "[WARN] $name is expected with $expected_image, but not deployed"
     continue
   fi
 
-  expected_image="${expected_versions[$name]:-}"
-
-  if [[ -z "$expected_image" ]]; then
-    echo "[WARN] $name is deployed with $deployed_image, but no expected image defined"
-
-  elif [[ "$expected_image" != "$deployed_image" ]]; then
+  if [[ "$expected_image" != "$deployed_image" ]]; then
     echo "[WARN] Mismatch in '$name': expected '$expected_image', found '$deployed_image'"
-
   else
     echo "[INFO] $name matches expected image version"
   fi
-done < "$deployed_file"
+done
+
+# Check if there are extra deployed containers not in expected
+for name in "${!deployed_versions[@]}"; do
+  if [[ -z "${expected_versions[$name]:-}" ]]; then
+    echo "[DEBUG] $name is deployed with ${deployed_versions[$name]}, but no expected image defined"
+  fi
+done
