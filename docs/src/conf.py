736c5f5e539588a810ef50ec5d5f3b321becc51c
David-mh <dhorn@sarao.ac.za>
2025-05-29 09:25:09 +0200

    AT-3250 update rc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.5.0rc10"
+release = "26.5.0rc12"
 
 # -- General configuration ---------------------------------------------------
 
