666fea379417bd2f638a5f562a952f63b59acb90
David-mh <dhorn@sarao.ac.za>
2025-05-30 11:56:47 +0200

    AT-3259 update rc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.5.0rc14"
+release = "26.5.0rc15"
 
 # -- General configuration ---------------------------------------------------
 
