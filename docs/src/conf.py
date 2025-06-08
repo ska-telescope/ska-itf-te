31d83045a1e88023ef6ecd2602f86881c775afc3
Adriaan de Beer <adebeer42@gmail.com>
2025-06-08 21:43:24 +0100

    AT-3259 bump RC

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "26.5.0rc15"
+release = "26.5.0rc16"
 
 # -- General configuration ---------------------------------------------------
 
