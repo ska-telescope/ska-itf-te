5276f5c8cf981d020446d4f01929d61f0909d975
Aditya Joshi <aditya.joshi@mda.space>
2025-07-17 17:22:24 -0700

    SKB-971 Update tests in RC 6

--- tests/integration/tmc/conftest.py
+++ tests/integration/tmc/conftest.py
@@ -440,6 +440,7 @@ def _(telescope_handlers, settings):
 
     _, _, csp, _ = telescope_handlers
     csp_control = csp.control
+    csp_subarray = csp.subarray
 
     assert csp_control.ping() > 0
 
@@ -462,6 +463,7 @@ def _(telescope_handlers, settings):
 
     csp_control.adminMode = 0
     wait_for_event(csp_control, "adminMode", 0)
+    csp_subarray.adminMode = 0
     sleep(15)  # TODO: Find out exactly why this is needed
 
     logger.info(
