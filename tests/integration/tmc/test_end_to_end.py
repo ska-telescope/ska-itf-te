bb94201dbf2c68bcc31a01095a8b932736fd19a7
Akhil-AJ <joseph_ajj@outlook.com>
2025-07-21 13:33:40 +0200

    AT-3306 new released-resources state check and test_e2e_via_tmc_slow_without_off

--- tests/integration/tmc/test_end_to_end.py
+++ tests/integration/tmc/test_end_to_end.py
@@ -6,6 +6,9 @@ import pytest
 from pytest_bdd import scenario, then
 from tango import DevState
 
+from utils.telescope_teardown import TelescopeState, TelescopeHandler
+from copy import deepcopy
+
 # TODO: Rethink usage of globals like this
 SUT_NAMESPACE = os.getenv("KUBE_NAMESPACE")
 
@@ -43,6 +46,42 @@ def test_e2e_via_tmc_slow_with_bite():
     if not (SUT_NAMESPACE := os.getenv("E2E_TEST_EXECUTION_NAMESPACE")):
         SUT_NAMESPACE = os.getenv("KUBE_NAMESPACE")
 
+@pytest.mark.hw_in_the_loop
+@scenario(
+    "features/tmc_end_to_end.feature",
+    "End to End signal chain verification via TMC without telescope OFF",
+)
+def test_e2e_via_tmc_slow_without_off():
+    """."""
+    global SUT_NAMESPACE
+
+    if not (SUT_NAMESPACE := os.getenv("E2E_TEST_EXECUTION_NAMESPACE")):
+        SUT_NAMESPACE = os.getenv("KUBE_NAMESPACE")
+
+@then("the telescope is in the released-resources state")
+def _(settings, receptor_ids):
+    """Check that the telescope is in the released-resources state.
+
+    :param settings: _description_
+    :type settings: _type_
+    :param receptor_ids: _description_
+    :type receptor_ids: _type_   
+    """
+
+    tmc_central_node = tmc.central_node
+
+    released_resources_state_1 = TelescopeState(
+        dishes={receptor_id: DishMode.OPERATE for receptor_id in receptor_ids})
+    released_resources_state_1.central_node = DevState.ON
+
+    # Due to known issue with state aggregation for telescopeState
+    released_resources_state_2 = deepcopy(released_resources_state_1)
+    released_resources_state_2.central_node = DevState.UNKNOWN
+
+    telescope_handler = TelescopeHandler(settings["SUT_namespace"], settings["sut_cluster_domain"], receptor_ids)
+    current_telescope_state = telescope_handler.get_telescope_state()
+
+    assert current_telescope_state in [released_resources_state, released_resources_state_2]
 
 @then("the telescope is in the OFF state")
 def _(telescope_handlers):
