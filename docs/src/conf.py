af64ffa3687d4835aa7cecf57ef45fdc127816a6
nnzotho <nnzotho@sarao.ac.za>
2025-07-28 11:38:14 +0200

    AT-3289 rc2

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "27.2.1rc1"
+release = "27.2.1rc2"
 
 # -- General configuration ---------------------------------------------------
 
