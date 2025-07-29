32671a12596867af0de6b5b9dc37e00991b36563
nnzotho <nnzotho@sarao.ac.za>
2025-07-29 17:20:50 +0200

    AT-3289 Resolved merge conflicts

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,11 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-<<<<<<< HEAD
 release = "27.2.1rc7"
-=======
-release = "27.2.0rc5"
->>>>>>> main
 
 # -- General configuration ---------------------------------------------------
 
