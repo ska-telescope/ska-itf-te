c5309efd29b50ce1da2bdacf111dfdb7bd47904c
David-mh <dhorn@sarao.ac.za>
2025-07-30 16:41:20 +0200

    AT-3341 - fixed lint error

--- tests/integration/resources/models/csp_model/entry_point.py
+++ tests/integration/resources/models/csp_model/entry_point.py
@@ -112,7 +112,8 @@ class StartUpStep(base.StartUpStep, LogEnabled, WithCommandID):
         dish_cfg = {
             "interface": "https://schema.skao.int/ska-mid-cbf-initsysparam/1.0",
             "tm_data_sources": ["car:ska-mid?main#tmdata"],
-            "tm_data_filepath": "instrument/ska1_mid_itf/vcc-config/ska-mid-cbf-system-parameters.json",
+            "tm_data_filepath": "instrument/ska1_mid_itf/vcc-config/"
+            "ska-mid-cbf-system-parameters.json",
         }
 
         load_cfg_id = self.csp_controller.command_inout("loadDishCfg", json.dumps(dish_cfg))
