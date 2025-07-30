12c4c9cf292e48ed8a5305b5b7f98dff44992e63
David-mh <dhorn@sarao.ac.za>
2025-07-30 09:31:14 +0200

    AT-3341 update entry_point.py with updated ska-mid-cbf-system-parameters file path

--- tests/integration/resources/models/csp_model/entry_point.py
+++ tests/integration/resources/models/csp_model/entry_point.py
@@ -112,10 +112,9 @@ class StartUpStep(base.StartUpStep, LogEnabled, WithCommandID):
         dish_cfg = {
             "interface": "https://schema.skao.int/ska-mid-cbf-initsysparam/1.0",
             "tm_data_sources": [
-                "car://gitlab.com/ska-telescope"
-                "/ska-telmodel-data?ska-sdp-tmlite-repository-1.0.0#tmdata"
+                "car://gitlab.com/ska-telescope/ska-mid/-/tree/main/tmdata"
             ],
-            "tm_data_filepath": "instrument/ska1_mid_psi/ska-mid-cbf-system-parameters.json",
+            "tm_data_filepath": "instrument/ska1_mid_itf/vcc-configgitlab.com/ska-telescope/ska-mid-cbf-system-parameters.json",
         }
 
         load_cfg_id = self.csp_controller.command_inout("loadDishCfg", json.dumps(dish_cfg))
