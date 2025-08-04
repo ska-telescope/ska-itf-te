8cd5a58e465ebeba6ae9cbd47185307991f667a3
Akhil-AJ <joseph_ajj@outlook.com>
2025-08-04 19:54:18 +0200

    AT-3253 RC

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "27.3.1rc2"
+release = "27.3.1rc3"
 
 # -- General configuration ---------------------------------------------------
 
