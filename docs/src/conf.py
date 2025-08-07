a21ef7088cffba14753bdec0332ae9740a5004fd
David-mh <dhorn@sarao.ac.za>
2025-08-07 22:10:18 +0200

    AT-3354 - at-3354 update rc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "27.3.1rc8"
+release = "27.3.1rc9"
 
 # -- General configuration ---------------------------------------------------
 
