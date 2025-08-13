01b389e2fb54fd51fd3104bc10320a649dfc7bc4
David-mh <dhorn@sarao.ac.za>
2025-08-13 16:29:19 +0200

    AT-3355 bump rc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "27.4.0rc6"
+release = "27.4.0rc7"
 
 # -- General configuration ---------------------------------------------------
 
