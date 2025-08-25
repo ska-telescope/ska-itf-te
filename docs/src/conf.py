335365ca6eaefff19771b7c4377731c0f25a20ea
Adriaan de Beer <adebeer42@gmail.com>
2025-08-25 16:29:29 +0200

    REL-1967 bump RC

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "27.5.0rc1"
+release = "27.5.0rc2"
 
 # -- General configuration ---------------------------------------------------
 
