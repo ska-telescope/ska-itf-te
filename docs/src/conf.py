4bb951e2748daa8bd40518536a13cb2b9a41a614
David-mh <dhorn@sarao.ac.za>
2025-08-26 09:30:15 +0200

    AT-3368 update rc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "27.5.0rc3"
+release = "27.5.0rc4"
 
 # -- General configuration ---------------------------------------------------
 
