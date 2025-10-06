df994dfb5476786e13e03d9ddf35db70259f48f8
Adriaan de Beer <adebeer42@gmail.com>
2025-10-06 17:24:50 +0200

    AT-3394 set release for dev tag

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "28.3.0"
+release = "28.3.1-dev-2"
 
 # -- General configuration ---------------------------------------------------
 
