1b322055fa511e0399d758b124b0263d4d90de25
David-mh <dhorn@sarao.ac.za>
2025-10-16 09:29:52 +0200

    AT-3407 - at-3407 set release tag to 28.3.3-static-delay-test-b

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "28.3.3-static-delay-test"
+release = "28.3.3-static-delay-test-b"
 
 # -- General configuration ---------------------------------------------------
 
