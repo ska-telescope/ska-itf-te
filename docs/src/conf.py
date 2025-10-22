a7dc207d541d11057873b40b3f78dfc88148d7ee
Akhil-AJ <joseph_ajj@outlook.com>
2025-10-22 17:24:32 +0200

    REL-2459 set 28.3.3

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "28.3.3rc3"
+release = "28.3.3"
 
 # -- General configuration ---------------------------------------------------
 
