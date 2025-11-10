3ae8e5b3534f605da9e7c3423adb97c5486b55de
Akhil-AJ <joseph_ajj@outlook.com>
2025-11-10 11:23:11 +0200

    AT-3431 Lint

--- tests/integration/tmc/test_individual_commands.py
+++ tests/integration/tmc/test_individual_commands.py
@@ -9,20 +9,24 @@ from pytest_bdd import scenario
 def test_assign_resources_via_tmc():
     """Assign resources via TMC."""
 
+
 @pytest.mark.hw_in_the_loop
 @scenario("features/test_individual_commands.feature", "Configure scan")
 def test_configure_scan_via_tmc():
     """Configure scan via TMC."""
+
 
 @pytest.mark.hw_in_the_loop
 @scenario("features/test_individual_commands.feature", "Scan")
 def test_scan_via_tmc():
     """Scan via TMC."""
 
+
 @pytest.mark.hw_in_the_loop
 @scenario("features/test_individual_commands.feature", "End the observation")
 def test_end_observation_via_tmc():
     """End the observation via TMC."""
+
 
 @pytest.mark.hw_in_the_loop
 @scenario("features/test_individual_commands.feature", "Release resources")
