ddf3596af14d5d2ab9cd9887d3cd8f37eb8499c5
Akhil-AJ <joseph_ajj@outlook.com>
2025-12-12 21:49:42 +0200

    AT-3465 Lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -55,10 +55,7 @@ def settings():
     settings["pointing_target_right_ascension"] = os.getenv("POINTING_TARGET_RIGHT_ASCENSION", "")
     settings["pointing_target_declination"] = os.getenv("POINTING_TARGET_DECLINATION", "")
     settings["telmodel_source"] = os.getenv("TELMODEL_SOURCE", "")
-    settings["dish_layout_telmodel_path"] = os.getenv(
-        "DISH_LAYOUT_TELMODEL_PATH",
-        "",
-    )
+    settings["dish_layout_telmodel_path"] = os.getenv("DISH_LAYOUT_TELMODEL_PATH", "")
     return settings
 
 
