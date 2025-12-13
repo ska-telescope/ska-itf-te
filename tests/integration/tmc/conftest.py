f4a5b9d658796c49a3e05706116ed48b2a4dea8f
Akhil-AJ <joseph_ajj@outlook.com>
2025-12-13 21:20:38 +0200

    AT-3465 Fix key

--- tests/integration/tmc/conftest.py
+++ tests/integration/tmc/conftest.py
@@ -1314,13 +1314,13 @@ def update_assign_resources(
         "nodeSelector"
     ] = node_selector_sdp_param
     if settings["telmodel_source"]:
-        assign_resources_payload["sdp"]["processing_blocks"][0]["parameters"]["extra_helm_values"]["receiver"]["options"]["telescope_model"][
-            "telmodel_source_uris"
-        ] = settings["telmodel_source"]
+        assign_resources_payload["sdp"]["processing_blocks"][0]["parameters"]["extra_helm_values"][
+            "receiver"
+        ]["options"]["telescope_model"]["telmodel_source_uris"] = settings["telmodel_source"]
     if settings["dish_layout_telmodel_path"]:
-        assign_resources_payload["sdp"]["processing_blocks"][0]["parameters"]["extra_helm_values"]["receiver"]["options"]["telescope_model"][
-            "telmodel_key"
-        ] = settings["dish_layout_telmodel_path"]
+        assign_resources_payload["sdp"]["processing_blocks"][0]["parameters"]["extra_helm_values"][
+            "receiver"
+        ]["options"]["telescope_model"]["telmodel_key"] = settings["dish_layout_telmodel_path"]
 
     return assign_resources_payload
 
