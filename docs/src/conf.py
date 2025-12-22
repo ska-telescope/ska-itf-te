2b455d3fda0921f9c6a01a66a9ca7f534c213922
Adriaan de Beer <adebeer42@gmail.com>
2025-12-22 17:00:33 +0200

    REL-2593 Bump RC

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.1.0rc2"
+release = "29.1.0rc3"
 
 # -- General configuration ---------------------------------------------------
 
