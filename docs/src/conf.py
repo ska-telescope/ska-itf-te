a08bac5866b554be752c92663532c1d565be6cd8
Adriaan de Beer <adebeer42@gmail.com>
2026-01-16 13:25:10 +0200

    REL-2596 bump minor version and created RC1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.1.0"
+release = "29.4.0rc1"
 
 # -- General configuration ---------------------------------------------------
 
