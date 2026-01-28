2f401f916e496c80962b27a292eb81455a6aa7ad
AshleyVivo <ashley@vivosa.co.za>
2026-01-28 09:06:55 +0200

    AT-3484 set test release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.4.0"
+release = "29.4.1-test-oso"
 
 # -- General configuration ---------------------------------------------------
 
