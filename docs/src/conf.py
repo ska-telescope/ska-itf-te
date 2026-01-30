2edc0ff2880f81d4bf2dcf8e61cde56856cc107a
David-mh <dhorn@sarao.ac.za>
2026-01-30 12:47:05 +0200

    AT-3476 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.4.1-test-at-3476"
+release = "29.4.1-at-3476b"
 
 # -- General configuration ---------------------------------------------------
 
