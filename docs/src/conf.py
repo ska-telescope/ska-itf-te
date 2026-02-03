7ccb78374d9501a6da4392a1768d6449b1d3932f
David-mh <dhorn@sarao.ac.za>
2026-02-03 10:19:20 +0200

    AT-3503 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.4.1-multi-es-b"
+release = "29.4.1-multi-es-c"
 
 # -- General configuration ---------------------------------------------------
 
