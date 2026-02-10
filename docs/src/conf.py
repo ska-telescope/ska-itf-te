368749ae1c30c241948603cde43334a8e3311279
AshleyVivo <ashley@vivosa.co.za>
2026-02-10 09:51:57 +0200

    REL-2597 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.4.1-multi-es-g"
+release = "29.5.0"
 
 # -- General configuration ---------------------------------------------------
 
