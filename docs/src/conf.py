de18e70aa55de3a9af21b9abd2e458390efa563e
David-mh <dhorn@sarao.ac.za>
2026-02-15 06:38:32 +0200

    AT-3528 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.5.2-test-staging-eda-c"
+release = "29.5.2-test-staging-eda-d"
 
 # -- General configuration ---------------------------------------------------
 
