1146a2a516652729e0eab0b024d98e7606becb74
AshleyVivo <ashley@vivosa.co.za>
2026-02-23 15:08:55 +0200

    AT-3511 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.5.2-test-testing-ns-b"
+release = "29.5.2-test-testing-ns-c"
 
 # -- General configuration ---------------------------------------------------
 
