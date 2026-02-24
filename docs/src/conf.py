306a68c1c8beac69a61eb285fe52aa31cd3c6de5
AshleyVivo <ashley@vivosa.co.za>
2026-02-24 14:47:31 +0200

    AT-3542 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.5.2-test-staging-eda-d"
+release = "29.5.2-test-b5dc"
 
 # -- General configuration ---------------------------------------------------
 
