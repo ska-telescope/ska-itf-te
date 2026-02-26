9e9eeaf7c6cc2fe3b1b4d245d7a7b2392800ec31
David-mh <dhorn@sarao.ac.za>
2026-02-26 11:27:59 +0200

    AT-3537 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "29.5.2-test-new-products-b"
+release = "29.5.2-test-new-products-c"
 
 # -- General configuration ---------------------------------------------------
 
