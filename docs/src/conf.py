03cf2754a99676a7bcdb7f34eca47ab2a8a0f7dd
David-mh <dhorn@sarao.ac.za>
2026-03-23 15:47:02 +0200

    AT-3580 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "30.1.0-test-oso-updates"
+release = "30.1.0-test-archiver-updates"
 
 # -- General configuration ---------------------------------------------------
 
