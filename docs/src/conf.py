98e6cfad6469465fcd35bc81cad2441e6d9b992b
David-mh <dhorn@sarao.ac.za>
2026-04-24 09:04:18 +0200

    AT-3648 merge in main

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "30.3.1-test-tmc"
+release = "30.3.1-test-tmc-b"
 
 # -- General configuration ---------------------------------------------------
 
