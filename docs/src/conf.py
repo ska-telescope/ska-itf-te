085c12d3418404345594e69eed233aa8ac707e7c
AshleyVivo <ashley@vivosa.co.za>
2026-05-04 16:48:05 +0200

    AT-3656 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "30.4.0-test-cbf-upgrades"
+release = "30.4.0-test-cbf-upgrades-b"
 
 # -- General configuration ---------------------------------------------------
 
