55c3a6d69a56b554b070fb7d61dce0487b523c10
AshleyVivo <ashley@vivosa.co.za>
2026-05-04 15:19:51 +0200

    AT-3656 rather use a test tag to be safe

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "30.4.0rc2"
+release = "30.4.0-test-cbf-upgrades"
 
 # -- General configuration ---------------------------------------------------
 
