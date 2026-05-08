43ae31146d26a83af0b8457a6cad522071f788e1
David-mh <dhorn@sarao.ac.za>
2026-05-08 15:33:33 +0200

    AT-3665 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "30.4.0rc2"
+release = "30.4.0rc4"
 
 # -- General configuration ---------------------------------------------------
 
