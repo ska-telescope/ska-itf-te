153e175a6221a499f83c41d70dc86be453575ac7
AshleyVivo <ashley@vivosa.co.za>
2026-05-14 11:41:30 +0200

    AT-3650 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "30.5.1-test-tmc-teapot-c"
+release = "30.5.1-test-tmc-teapot-d"
 
 # -- General configuration ---------------------------------------------------
 
