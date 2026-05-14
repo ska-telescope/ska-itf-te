dff50dd95eea30f6ee79d4ea77a7d67953dd4711
susanspangenberg <susan@tsolo.io>
2026-05-14 15:17:06 +0200

    AT-3650 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "30.5.1-test-tmc-teapot-d"
+release = "30.5.1-test-tmc-teapot-e"
 
 # -- General configuration ---------------------------------------------------
 
