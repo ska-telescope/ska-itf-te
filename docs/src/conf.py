28de5289299ce62ad88984501e476d77abd9c249
AshleyVivo <ashley@vivosa.co.za>
2026-06-12 14:10:43 +0200

    AT-3708

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.1.0-test-tmc-1-21"
+release = "31.1.0-test-tmc-1-21-b"
 
 # -- General configuration ---------------------------------------------------
 
