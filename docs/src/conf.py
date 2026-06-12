bbb662b76b4d637e478ca6f05c7e3c454c718b55
AshleyVivo <ashley@vivosa.co.za>
2026-06-12 16:11:51 +0200

    AT-3708 create tag for scicomm

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.1.0-test-tmc-1-21-b"
+release = "31.1.0-5-point-holo"
 
 # -- General configuration ---------------------------------------------------
 
