f7769705e0e99e7053fdfa749dee7f9b298fd3d8
David-mh <dhorn@sarao.ac.za>
2026-06-12 12:44:17 +0200

    AT-3704 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.1.0-test-product-updates-b"
+release = "31.1.0-test-product-updates-c"
 
 # -- General configuration ---------------------------------------------------
 
