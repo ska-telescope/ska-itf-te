fa49e8549003b4219a133f70146f5ebb94e02778
David-mh <dhorn@sarao.ac.za>
2026-06-16 13:21:00 +0200

    AT-3711 set release tag

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.0.0"
+release = "31.0.1-test-tangogql"
 
 # -- General configuration ---------------------------------------------------
 
