9372a9c8081e464499667786a24a7ede7ef27ed4
Adriaan de Beer <adebeer42@gmail.com>
2026-06-22 12:43:37 +0200

    AT-3715 Bump RC

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.2.0"
+release = "31.2.0rc1"
 
 # -- General configuration ---------------------------------------------------
 
