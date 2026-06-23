d10c4c434a44d0d0873884a3c9455f6e7323e03b
AshleyVivo <ashley@vivosa.co.za>
2026-06-23 15:21:56 +0200

    AT-3714 set release for testing

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.2.0-test-oso-upgrades-b"
+release = "31.2.0-test-oso-upgrades-c"
 
 # -- General configuration ---------------------------------------------------
 
