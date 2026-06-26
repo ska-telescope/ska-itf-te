e36a322a91a7a4e3cc4e7fd28951198be635ca82
David-mh <dhorn@sarao.ac.za>
2026-06-26 12:54:10 +0200

    AT-3726 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.3.0-test-spfrx-3-1-0"
+release = "31.3.0-test-spfrx-3-1-0-b"
 
 # -- General configuration ---------------------------------------------------
 
