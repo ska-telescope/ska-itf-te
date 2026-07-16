2423ce4b23ce7c666b8a973e222b1f0add160a1f
susanspangenberg <susan@tsolo.io>
2026-07-16 14:42:22 +0200

    AT-3745 new test release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.2.3-test-upgradability-e"
+release = "31.2.3-test-upgradability-f"
 
 # -- General configuration ---------------------------------------------------
 
