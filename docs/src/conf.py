a7a8086cb89bf1f2e25761d0cfed1c18be1ba58a
AshleyVivo <ashley@vivosa.co.za>
2026-07-22 16:51:28 +0200

    AT-3756 revert release back to main

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.3.0-test-dish-manager-fix-c"
+release = "31.2.2"
 
 # -- General configuration ---------------------------------------------------
 
