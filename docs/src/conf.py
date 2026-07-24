fa5040448dd79f8aa6f7f29d60c6dded8cf21881
David-mh <dhorn@sarao.ac.za>
2026-07-24 15:17:59 +0200

    AT-3763 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.4.0"
+release = "31.4.1-vcc-swap"
 
 # -- General configuration ---------------------------------------------------
 
