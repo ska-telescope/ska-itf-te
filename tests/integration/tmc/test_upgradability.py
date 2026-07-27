1aeeda6923a65d07d58ae683764553622c3e28c0
susanspangenberg <susan@tsolo.io>
2026-07-27 15:36:14 +0200

    AT-3758 register new marker for upgradability test

--- tests/integration/tmc/test_upgradability.py
+++ tests/integration/tmc/test_upgradability.py
@@ -1,13 +1,13 @@
 """Upgrade path test via TMC feature tests."""
 
-# import pytest
+import pytest
 from pytest_bdd import scenario
 
 
-# @pytest.mark.hw_in_the_loop
+@pytest.mark.upgrade_path
 @scenario(
     "features/upgradability.feature",
     "Test upgrade path from the current version of SKA Mid running in Production to the current new tag",  # noqa: E501
 )
 def test_upgrade_path():
-    """."""
+    """Validate upgrade path from previous production version."""
