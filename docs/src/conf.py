087fe302d98fffdf527cc39521cdf252014fadca
David-mh <dhorn@sarao.ac.za>
2026-08-03 08:42:26 +0200

    AT-3767 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.4.1-test-sdp-2-6-1"
+release = "31.4.1-test-sdp-2-6-1b"
 
 # -- General configuration ---------------------------------------------------
 
