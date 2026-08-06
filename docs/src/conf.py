85833ebf4730022923b81138094458fd56e392a6
susanspangenberg <susan@tsolo.io>
2026-08-06 10:17:37 +0200

    AT-3758 new fake release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.4.1-test-upgradability-i"
+release = "31.4.1-test-upgradability-j"
 
 # -- General configuration ---------------------------------------------------
 
