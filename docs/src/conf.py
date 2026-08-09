9cf1a4b0b12cea1acfaefef0378a835b308b0217
Adriaan de Beer <adebeer42@gmail.com>
2026-08-09 08:08:23 +0200

    AT-3796 set release to test version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.5.1"
+release = "31.5.1-test-secrets"
 
 # -- General configuration ---------------------------------------------------
 
