94a664f7d1042cc7aa313dab6c6e10495ce8acaa
David-mh <dhorn@sarao.ac.za>
2026-08-11 13:58:51 +0200

    AT-3773 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.5.0-k-value-1111-test-a"
+release = "31.5.0-k-value-1111-test-b"
 
 # -- General configuration ---------------------------------------------------
 
