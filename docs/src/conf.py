428f16cd843e24cec56ce49fc339cec09316251e
David-mh <dhorn@sarao.ac.za>
2026-08-24 09:12:20 +0200

    SKB-1061 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.5.1-skb-1061-test"
+release = "31.5.1-skb-1061-test-b"
 
 # -- General configuration ---------------------------------------------------
 
