501026a9b70b5f22708272ea004fc61b9c66d377
David-mh <dhorn@sarao.ac.za>
2026-08-24 13:29:57 +0200

    AT-3650 set release

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.5.1-test-teapot-a"
+release = "31.5.2-test-teapot-a"
 
 # -- General configuration ---------------------------------------------------
 
