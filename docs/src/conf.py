571a7be383bf8c171b404447758a8150acf10133
David-mh <dhorn@sarao.ac.za>
2026-08-26 16:13:46 +0200

    AT-3812 set release back to what is on main

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,7 +29,7 @@ author = "ATLAS Team"
 
 # The full version, including alpha/beta/rc tags
 
-release = "31.5.2-test-slew"
+release = "31.5.2"
 
 # -- General configuration ---------------------------------------------------
 
